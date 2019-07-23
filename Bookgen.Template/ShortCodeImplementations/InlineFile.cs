083098f9f62c885160c55d87336fa5f5f47929ff
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2019-07-23 21:42:11 +0200

    ShortCodes updated

--- Bookgen.Template/ShortCodeImplementations/InlineFile.cs
+++ Bookgen.Template/ShortCodeImplementations/InlineFile.cs
@@ -3,21 +3,25 @@
 // This code is licensed under MIT license (see LICENSE for details)
 //-----------------------------------------------------------------------------
 
+using BookGen.Core;
 using BookGen.Core.Contracts;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 
 namespace Bookgen.Template.ShortCodeImplementations
 {
     [Export(typeof(ITemplateShortCode))]
-    public class BuildTime : ITemplateShortCode
+    public class InlineFile : ITemplateShortCode
     {
-        public string Tag => nameof(BuildTime);
+        public string Tag => nameof(InlineFile);
 
         public string Generate(IReadOnlyDictionary<string, string> arguments)
         {
-            return DateTime.Now.ToString("yy-MM-dd hh:mm:ss");
+           var name = arguments.GetArgumentOrThrow("file");
+
+            FsPath file = new FsPath(name);
+
+            return file.ReadFile();
         }
     }
 }
