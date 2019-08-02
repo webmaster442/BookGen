e4ec1da43535fe1d077bac44a4ab9b4e33ffc910
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2019-08-02 19:39:19 +0200

    SRI generator & tests

---
+++ BookGen/Framework/SRIGenerator.cs
@@ -0,0 +1,40 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2019 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using BookGen.Core;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BookGen.Framework
+{
+    internal static class SriGenerator
+    {
+        public static string GetSRI(string content)
+        {
+            using (var hashAlgorithm = new SHA384Managed())
+            {
+
+                byte[] textData = Encoding.UTF8.GetBytes(content);
+                byte[] hash = hashAlgorithm.ComputeHash(textData);
+
+                return "sha384-" + Convert.ToBase64String(hash);
+            }
+        }
+
+        public static string GetSRI(FsPath path)
+        {
+            using (var fs = File.OpenRead(path.ToString()))
+            {
+                using (var hashAlgorithm = new SHA384Managed())
+                {
+                    byte[] hash = hashAlgorithm.ComputeHash(fs);
+                    return "sha384-" + Convert.ToBase64String(hash);
+                }
+            }
+        }
+    }
+}
