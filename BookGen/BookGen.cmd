ee3fd437ceccb80cadd95558af9fa9f250838112
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2019-08-05 22:26:39 +0200

    New Publish script & launcher scripts

---
+++ BookGen/BookGen.cmd
@@ -0,0 +1,7 @@
+@echo off
+rem -----------------------------------------------------------------------------
+rem  (c) 2019 Ruzsinszki Gábor
+rem  This code is licensed under MIT license (see LICENSE for details)
+rem -----------------------------------------------------------------------------
+
+dotnet BookGen.dll %*
