0f27ea2efaef44f8c40a2277b32e013d2ab96094
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2019-08-05 22:35:14 +0200

    Updated launch scripts with runtime detection

--- BookGen/BookGen.cmd
+++ BookGen/BookGen.cmd
@@ -4,4 +4,13 @@ rem  (c) 2019 Ruzsinszki Gábor
 rem  This code is licensed under MIT license (see LICENSE for details)
 rem -----------------------------------------------------------------------------
 
+WHERE foo 2> nul
+IF %ERRORLEVEL% NEQ 0 goto notfound
+
 dotnet BookGen.dll %*
+goto exit
+
+:notfound
+ECHO This program Requires .NET Core Runtime, but it's not installed. Exiting
+
+:exit
