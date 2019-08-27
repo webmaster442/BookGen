9518c5d0ce26b0f9eafd12963af0f10aab327ce4
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2019-08-27 16:58:45 +0200

    NHuspell services 0. version

---
+++ BookGen.Editor/Services/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+
+namespace BookGen.Editor.Services
+{
+    public class NHuspellServices
+    {
+        private const string BaseUrl = "https://github.com/LibreOffice/dictionaries/{0}/{0}.{1}";
+
+        public async Task DownloadDictionary(string code, string target)
+        {
+            using (var client = new WebClient())
+            {
+                var aff = string.Format(BaseUrl, code, "aff");
+                var dic = string.Format(BaseUrl, code, "dic");
+                await client.DownloadFileTaskAsync(aff, target).ConfigureAwait(false);
+                await client.DownloadFileTaskAsync(dic, target).ConfigureAwait(false);
+            }
+        }
+    }
+}
