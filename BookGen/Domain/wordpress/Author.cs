9115b9456b742cd9a7a631033adcb7eaccfe6977
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2019-08-27 20:44:14 +0200

    Wordpress export file domain objects added

---
+++ BookGen/Domain/wordpress/Author.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2019 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using System.Xml.Serialization;
+
+namespace BookGen.Domain.wordpress
+{
+    [XmlRoot(ElementName = "author", Namespace = "http://wordpress.org/export/1.2/")]
+    public class Author
+    {
+        [XmlElement(ElementName = "author_id", Namespace = "http://wordpress.org/export/1.2/")]
+        public string Author_id { get; set; }
+        [XmlElement(ElementName = "author_login", Namespace = "http://wordpress.org/export/1.2/")]
+        public string Author_login { get; set; }
+        [XmlElement(ElementName = "author_email", Namespace = "http://wordpress.org/export/1.2/")]
+        public string Author_email { get; set; }
+        [XmlElement(ElementName = "author_display_name", Namespace = "http://wordpress.org/export/1.2/")]
+        public string Author_display_name { get; set; }
+        [XmlElement(ElementName = "author_first_name", Namespace = "http://wordpress.org/export/1.2/")]
+        public string Author_first_name { get; set; }
+        [XmlElement(ElementName = "author_last_name", Namespace = "http://wordpress.org/export/1.2/")]
+        public string Author_last_name { get; set; }
+    }
+}
---
+++ BookGen/Domain/wordpress/Guid.cs
@@ -0,0 +1,18 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2019 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using System.Xml.Serialization;
+
+namespace BookGen.Domain.wordpress
+{
+    [XmlRoot(ElementName = "guid")]
+    public class Guid
+    {
+        [XmlAttribute(AttributeName = "isPermaLink")]
+        public string IsPermaLink { get; set; }
+        [XmlText]
+        public string Text { get; set; }
+    }
+}
