f011f482fe34fa787ef9b731fa36fd75de6744e1
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2019-09-01 18:47:08 +0200

    FSPath inteligent \ and / handling

--- BookGen.Core/FsPath.cs
+++ BookGen.Core/FsPath.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace BookGen.Core
 {
@@ -15,7 +16,34 @@ namespace BookGen.Core
 
         public FsPath(params string[] pathParts)
         {
-            _path = Path.Combine(pathParts);
+            if (pathParts == null)
+                throw new ArgumentNullException(nameof(pathParts));
+
+            if (pathParts.Length < 1)
+                throw new ArgumentException($"{nameof(pathParts)} must contain at least one element");
+
+            var combined = Path.Combine(pathParts);
+
+            if (combined.Contains("/") && combined.Contains("\\"))
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
+                    || RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    _path = combined.Replace("\\", "/");
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    _path = combined.Replace("/", "\\");
+                }
+                else
+                {
+                    _path = combined;
+                }
+            }
+            else
+            {
+                _path = combined;
+            }
         }
 
         public FsPath Combine(string part)
