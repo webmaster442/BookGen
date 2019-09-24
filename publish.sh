fb971624f8b2a5e11ec0ac7a684ad03bc73786c8
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2019-09-24 21:15:03 +0200

    Publish script update

--- publish.sh
+++ publish.sh
@@ -4,17 +4,16 @@
 # This code is licensed under MIT license (see LICENSE for details)
 #-----------------------------------------------------------------------------
 
-function build
+function build-core3
 {
 	printf "\nBuilding BookGen ..."
-	dotnet publish -c Release -f netcoreapp2.2 -o ../bin/BookGen
-	cd bin
-	cd BookGen
+	dotnet publish -c Release -f -f netcoreapp3.0 -o ./bin/BookGen
+	cd bin/BookGen
 	rm *.pdb
 	cd ..
 	tar -zcvf BookGen.tar.gz BookGen
-	rm -r BookGen
 	cd ..
+	printf "\nFinished. Package Path: bin/BookGen.tar.gz"
 }
 
-build
+build-core3
