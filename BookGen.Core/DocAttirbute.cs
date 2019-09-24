a761380feb96b2710eba1a77c026a7d6829ec407
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2019-09-24 17:52:32 +0200

    DocAttribute for documenting config

---
+++ BookGen.Core/DocAttirbute.cs
@@ -0,0 +1,28 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2019 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using System;
+
+namespace BookGen.Core
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class DocAttribute : Attribute
+    {
+        public bool IsOptional { get; set; }
+        public string Description { get; set; }
+
+        public DocAttribute(string description)
+        {
+            Description = description;
+            IsOptional = false;
+        }
+
+        public DocAttribute(string description, bool optional)
+        {
+            Description = description;
+            IsOptional = optional;
+        }
+    }
+}
