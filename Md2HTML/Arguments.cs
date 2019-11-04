528e6dcd98557330e74c46d7edcf16c0360798d8
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2019-11-04 22:33:04 +0100

    Md2HTML skeleton

---
+++ Md2HTML/Arguments.cs
@@ -0,0 +1,9 @@
+﻿namespace Md2HTML
+{
+    internal class Arguments
+    {
+        public string InputFile { get; set; }
+        public string OutputFile { get; set; }
+        public string Css { get; set; }
+    }
+}
---
+++ Md2HTML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using BookGen.Core;
+
+namespace Md2HTML
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
