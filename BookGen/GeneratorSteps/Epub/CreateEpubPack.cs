d7955a32cd88a8238734e9571a1c13dbf4f1b240
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2019-12-08 18:35:53 +0100

    Epub files are now correctly compressed

--- BookGen/GeneratorSteps/Epub/CreateEpubPack.cs
+++ BookGen/GeneratorSteps/Epub/CreateEpubPack.cs
@@ -4,8 +4,10 @@
 //-----------------------------------------------------------------------------
 
 using BookGen.Contracts;
+using BookGen.Core;
 using BookGen.Core.Contracts;
 using BookGen.Domain;
+using System.IO;
 using System.IO.Compression;
 
 namespace BookGen.GeneratorSteps.Epub
@@ -15,13 +17,32 @@ namespace BookGen.GeneratorSteps.Epub
         public void RunStep(RuntimeSettings settings, ILog log)
         {
             log.Info("Creating epub file from contents...");
-            var output = settings.OutputDirectory.Combine("book.epub");
-            var input = settings.OutputDirectory.Combine("epubtemp");
+            FsPath output = settings.OutputDirectory.Combine("book.epub");
+            FsPath input = settings.OutputDirectory.Combine("epubtemp");
 
-            ZipFile.CreateFromDirectory(input.ToString(),
-                                        output.ToString(),
-                                        CompressionLevel.NoCompression,
-                                        false);
+            output.WriteFile(log, "");
+
+            string[] files = input.GetAllFiles();
+
+            int removeLength = input.ToString().Length + 1;
+
+            using (var fs = File.Create(output.ToString()))
+            {
+                using (var zip = new ZipArchive(fs, ZipArchiveMode.Create))
+                {
+                    //note: 1st entry is mimetype. It must not be compressed for correct epub export
+                    zip.CreateEntryFromFile(files[0], GetEntryName(files[0], removeLength), CompressionLevel.NoCompression);
+                    for (int i=1; i<files.Length; i++)
+                    {
+                        zip.CreateEntryFromFile(files[i], GetEntryName(files[i], removeLength), CompressionLevel.Optimal);
+                    }
+                }
+            }
+        }
+
+        private string GetEntryName(string fileName, int rootFolderstringLength)
+        {
+            return fileName.Substring(rootFolderstringLength).Replace("\\", "/");
         }
     }
 }
