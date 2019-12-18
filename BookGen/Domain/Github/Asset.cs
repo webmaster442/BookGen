4c8248f9d8bdc2043099dbdc66437aec07d89b83
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2019-12-18 17:59:02 +0100

    Github API classes for updater

---
+++ BookGen/Domain/Github/Asset.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BookGen.Domain.Github
+{
+    internal class Asset
+    {
+        [JsonPropertyName("content_type")]
+        public string? ContentType { get; set; }
+
+        [JsonPropertyName("browser_download_url")]
+        public string? DownloadUrl { get; set; }
+
+        [JsonPropertyName("size")]
+        public long Size { get; set; }
+    }
+}
---
+++ BookGen/Domain/Github/Relese.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+
+namespace BookGen.Domain.Github
+{
+    internal class Relese
+    {
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("published_at")]
+        public DateTime? PublishDate { get; set; }
+
+        [JsonPropertyName("prerelease")]
+        public bool IsPreRelase { get; set; }
+
+        [JsonPropertyName("draft")]
+        public bool IsDraft { get; set; }
+
+        [JsonPropertyName("assets")]
+        public List<Asset>? Assets { get; set; }
+
+        [JsonPropertyName("body")]
+        public string? Body { get; set; }
+    }
+}
