3c73af4a745b38f4055b427c6532b04fc3a7fdec
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2019-12-27 16:22:18 +0100

    Adapted shortcode parser tests

--- BookGen.Tests/UT_ShortCodeParser.cs
+++ BookGen.Tests/UT_ShortCodeParser.cs
@@ -36,14 +36,14 @@ namespace BookGen.Tests
         [Test]
         public void EnshureThat_ShortCodeParser_Parse_Works_ForSimple()
         {
-            var result = _sut.Parse("<<Dumy>>");
+            var result = _sut.Parse("<!--{Dumy}-->");
             Assert.AreEqual("Genrated", result);
         }
 
         [Test]
         public void EnshureThat_ShortCodeParser_Parse_Works_ForComplexStrings()
         {
-            var input = "Lorem ipsum dolor sit <<Dumy>>, consectetur adipiscing elit. Nam non porttitor ligula. Proin eget pulvinar nisi. Suspendisse urna quam, vehicula nec felis eget, faucibus vestibulum diam. Etiam ultrices dignissim laoreet. Cras porttitor, nisi sit amet commodo porttitor, enim felis tempus eros, ac mattis sapien ante sed nunc. Praesent sodales porttitor nisi in dictum. Proin ut sapien turpis. Mauris mattis aliquet condimentum. Nullam imperdiet libero sit amet risus placerat, in eleifend arcu vestibulum. Morbi aliquam rutrum turpis, sit amet ultrices lorem tempor sit amet. Curabitur mollis placerat mi, ut auctor dui bibendum in. Aenean quis placerat lorem, <<Dumy>> mollis elit. Proin.";
+            var input = "Lorem ipsum dolor sit <!--{Dumy}-->, consectetur adipiscing elit. Nam non porttitor ligula. Proin eget pulvinar nisi. Suspendisse urna quam, vehicula nec felis eget, faucibus vestibulum diam. Etiam ultrices dignissim laoreet. Cras porttitor, nisi sit amet commodo porttitor, enim felis tempus eros, ac mattis sapien ante sed nunc. Praesent sodales porttitor nisi in dictum. Proin ut sapien turpis. Mauris mattis aliquet condimentum. Nullam imperdiet libero sit amet risus placerat, in eleifend arcu vestibulum. Morbi aliquam rutrum turpis, sit amet ultrices lorem tempor sit amet. Curabitur mollis placerat mi, ut auctor dui bibendum in. Aenean quis placerat lorem, <!--{Dumy}--> mollis elit. Proin.";
             var expected = "Lorem ipsum dolor sit Genrated, consectetur adipiscing elit. Nam non porttitor ligula. Proin eget pulvinar nisi. Suspendisse urna quam, vehicula nec felis eget, faucibus vestibulum diam. Etiam ultrices dignissim laoreet. Cras porttitor, nisi sit amet commodo porttitor, enim felis tempus eros, ac mattis sapien ante sed nunc. Praesent sodales porttitor nisi in dictum. Proin ut sapien turpis. Mauris mattis aliquet condimentum. Nullam imperdiet libero sit amet risus placerat, in eleifend arcu vestibulum. Morbi aliquam rutrum turpis, sit amet ultrices lorem tempor sit amet. Curabitur mollis placerat mi, ut auctor dui bibendum in. Aenean quis placerat lorem, Genrated mollis elit. Proin.";
             var result = _sut.Parse(input);
             Assert.AreEqual(expected, result);
@@ -60,14 +60,14 @@ namespace BookGen.Tests
         [Test]
         public void EnshureThat_ShortCodeParser_Parse_Works_ForArgumented()
         {
-            var result = _sut.Parse("<<Arguments parameter=\"success\">>");
+            var result = _sut.Parse("<!--{Arguments parameter=\"success\"}-->");
             Assert.AreEqual("success", result);
         }
 
         [Test]
         public void EnshureThat_ShortCodeParser_Parse_Works_ForFilePath()
         {
-            var result = _sut.Parse("<<Arguments parameter=\"c:\\asd folder\foo.txt\">>");
+            var result = _sut.Parse("<!--{Arguments parameter=\"c:\\asd folder\foo.txt\"}-->");
             Assert.AreEqual("c:\\asd folder\foo.txt", result);
         }
 
@@ -76,7 +76,7 @@ namespace BookGen.Tests
         [TestCase("##")]
         public void EnshureThat_ShortCodeParser_Parse_HandlesArgumentsWithoutValue(string input)
         {
-            var result = _sut.Parse($"<<yield {input}>>");
+            var result = _sut.Parse($"<!--{{yield {input}}}-->");
             Assert.AreEqual(input, result);
         }
     }
