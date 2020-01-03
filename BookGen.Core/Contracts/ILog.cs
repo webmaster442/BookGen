fe724a943665aba40b975946eb41fc7463c37a04
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2020-01-03 09:28:06 +0100

    Log interface simplified with default implementation in interface

--- BookGen.Core/Contracts/ILog.cs
+++ BookGen.Core/Contracts/ILog.cs
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------------
-// (c) 2019 Ruzsinszki Gábor
+// (c) 2019-2020 Ruzsinszki Gábor
 // This code is licensed under MIT license (see LICENSE for details)
 //-----------------------------------------------------------------------------
 
@@ -7,14 +7,23 @@ using System;
 
 namespace BookGen.Core.Contracts
 {
+    /// <summary>
+    /// Interface for logging
+    /// </summary>
     public interface ILog
     {
         void Log(LogLevel logLevel, string format, params object[] args);
-        void Critical(string format, params object[] args);
-        void Critical(Exception ex);
-        void Warning(string format, params object[] args);
-        void Warning(Exception ex);
-        void Info(string format, params object[] args);
-        void Detail(string format, params object[] args);
+        void Critical(string format, params object[] args) 
+            => Log(LogLevel.Critical, format, args);
+        void Critical(Exception ex)
+            => Log(LogLevel.Critical, "{0}\r\n{1}", ex.Message, ex.StackTrace);
+        void Warning(string format, params object[] args)
+            => Log(LogLevel.Warning, format, args);
+        void Warning(Exception ex)
+            => Log(LogLevel.Warning, "{0}", ex.Message);
+        void Info(string format, params object[] args)
+            => Log(LogLevel.Info, format, args);
+        void Detail(string format, params object[] args)
+            => Log(LogLevel.Info, format, args);
     }
 }
