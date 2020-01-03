197711d78b64cd990f133a9e355735f5d53c76c9
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2020-01-03 09:21:01 +0100

    Moved script interface to Core project

--- BookGen.Core/Contracts/IScript.cs
+++ BookGen.Core/Contracts/IScript.cs
@@ -1,12 +1,11 @@
 ﻿//-----------------------------------------------------------------------------
-// (c) 2019 Ruzsinszki Gábor
+// (c) 2019-2020 Ruzsinszki Gábor
 // This code is licensed under MIT license (see LICENSE for details)
 //-----------------------------------------------------------------------------
 
-using BookGen.Core.Contracts;
 using System.Collections.Generic;
 
-namespace BookGen.Contracts
+namespace BookGen.Core.Contracts
 {
     /// <summary>
     /// Interface for scripts.
