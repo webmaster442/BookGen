b099ff7c8d84af95117a98b122662a6ed7483f97
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2020-01-06 18:38:42 +0100

    Script host interface added

--- BookGen.Api/IScriptHost.cs
+++ BookGen.Api/IScriptHost.cs
@@ -3,11 +3,23 @@
 // This code is licensed under MIT license (see LICENSE for details)
 //-----------------------------------------------------------------------------
 
+using BookGen.Api.Configuration;
+
 namespace BookGen.Api
 {
     public interface IScriptHost
     {
+        /// <summary>
+        /// Source directory of input files
+        /// </summary>
         string SourceDirectory { get; }
-        string TargetDirectory { get; }
+        /// <summary>
+        /// Current configuration
+        /// </summary>
+        IReadOnlyConfig Configuration { get; }
+        /// <summary>
+        /// Currently active build configuration
+        /// </summary>
+        IReadOnlyBuildConfig CurrentBuildConfig { get; }
     }
 }
