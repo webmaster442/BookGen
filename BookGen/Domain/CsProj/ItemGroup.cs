7cb501ba5adbc2ff07bfde5eeb689dc7b647a8ee
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2020-01-06 19:36:20 +0100

    Added CsProj domain classes

---
+++ BookGen/Domain/CsProj/ItemGroup.cs
@@ -0,0 +1,16 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2020 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using System.Xml.Serialization;
+
+namespace BookGen.Domain.CsProj
+{
+    [XmlRoot(ElementName = "ItemGroup")]
+    public class ItemGroup
+    {
+        [XmlElement(ElementName = "Reference")]
+        public Reference? Reference { get; set; }
+    }
+}
