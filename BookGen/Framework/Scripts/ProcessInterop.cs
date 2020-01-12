1ad6680c334b50191cc4057ca46009c63d162d4c
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2020-01-12 18:42:31 +0100

    Process Interop helper

---
+++ BookGen/Framework/Scripts/ProcessInterop.cs
@@ -0,0 +1,125 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2020 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using System;
+using System.Diagnostics;
+using System.IO.MemoryMappedFiles;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookGen.Framework.Scripts
+{
+    internal static class ProcessInterop
+    {
+        private static Task<bool> WaitForExitAsync(Process process, int timeout)
+        {
+            return Task.Run(() => process.WaitForExit(timeout));
+        }
+
+        private static async Task<(int exitcode, string output)> ExecuteShellCommand(string command, string arguments, int timeout)
+        {
+            (int exitcode, string output) result = (-1, string.Empty);
+
+            using (var process = new Process())
+            {
+                // If you run bash-script on Linux it is possible that ExitCode can be 255.
+                // To fix it you can try to add '#!/bin/bash' header to the script.
+
+                process.StartInfo.FileName = command;
+                process.StartInfo.Arguments = arguments;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardInput = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.CreateNoWindow = true;
+
+                var outputBuilder = new StringBuilder();
+                var outputCloseEvent = new TaskCompletionSource<bool>();
+
+                process.OutputDataReceived += (s, e) =>
+                {
+                    // The output stream has been closed i.e. the process has terminated
+                    if (e.Data == null)
+                        outputCloseEvent.SetResult(true);
+                    else
+                        outputBuilder.AppendLine(e.Data);
+                };
+
+                var errorBuilder = new StringBuilder();
+                var errorCloseEvent = new TaskCompletionSource<bool>();
+
+                process.ErrorDataReceived += (s, e) =>
+                {
+                    // The error stream has been closed i.e. the process has terminated
+                    if (e.Data == null)
+                        errorCloseEvent.SetResult(true);
+                    else
+                        errorBuilder.AppendLine(e.Data);
+                };
+
+                bool isStarted;
+
+                try
+                {
+                    isStarted = process.Start();
+                }
+                catch (Exception error)
+                {
+                    // Usually it occurs when an executable file is not found or is not executable
+
+                    result.exitcode = -1;
+                    result.output = error.Message;
+                    isStarted = false;
+                }
+
+                if (!isStarted)
+                    return result;
+
+                // Reads the output stream first and then waits because deadlocks are possible
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                // Creates task to wait for process exit using timeout
+                var waitForExit = WaitForExitAsync(process, timeout);
+
+                // Create task to wait for process exit and closing all output streams
+                var processTask = Task.WhenAll(waitForExit, outputCloseEvent.Task, errorCloseEvent.Task);
+
+                // Waits process completion and then checks it was not completed by timeout
+                if (await Task.WhenAny(Task.Delay(timeout), processTask) == processTask && waitForExit.Result)
+                {
+                    result.exitcode = process.ExitCode;
+
+                    // Adds process output if it was completed with error
+                    if (process.ExitCode != 0)
+                    {
+                        result.output = $"{outputBuilder}{errorBuilder}";
+                    }
+
+                    return result;
+                }
+
+                try
+                {
+                    // Kill hung process
+                    process.Kill();
+                }
+                catch
+                {
+                    //Intentionally left empty
+                }
+            }
+            return result;
+        }
+
+        public static MemoryMappedFile CreateFile(string location, string contents)
+        {
+            MemoryMappedFile mmf = MemoryMappedFile.CreateNew("test", contents.Length * 2);
+            MemoryMappedViewAccessor accessor = mmf.CreateViewAccessor();
+            accessor.WriteArray(0, contents.ToCharArray(), 0, contents.Length);
+            return mmf;
+        }
+    }
+}
