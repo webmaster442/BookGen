60c7d8dcaea6c7dab2b86e728de754d5e1f81d4f
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2020-01-12 19:59:08 +0100

    Python script host

--- BookGen.Core/JsonInliner.cs
+++ BookGen.Core/JsonInliner.cs
@@ -5,6 +5,7 @@
 
 using BookGen.Api;
 using System;
+using System.Text;
 using System.Text.Json;
 
 namespace BookGen.Core
@@ -22,6 +23,27 @@ namespace BookGen.Core
             catch (Exception ex)
             {
                 log.Warning("InlineJs failed. type: {1}", typeof(T));
+                log.Detail(ex.Message);
+                return string.Empty;
+            }
+        }
+
+        public static string InlinePython<T>(string variableName, T obj, ILog log) where T : class
+        {
+            try
+            {
+                var json = JsonSerializer.Serialize<T>(obj);
+                var pythonBuilder = new StringBuilder(4096);
+                pythonBuilder.AppendLine("import json");
+                pythonBuilder.AppendLine("class Deserialize(object):");
+                pythonBuilder.AppendLine("    def __init__(self, j):");
+                pythonBuilder.AppendLine("          self.__dict__ = json.loads(j)");
+                pythonBuilder.AppendFormat("{0} = Deserialize('{1}')\r\n", variableName, json);
+                return pythonBuilder.ToString();
+            }
+            catch (Exception ex)
+            {
+                log.Warning("InlinePython failed. type: {1}", typeof(T));
                 log.Detail(ex.Message);
                 return string.Empty;
             }
---
+++ BookGen/Framework/Scripts/PtyhonHost.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2020 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using BookGen.Api;
+using BookGen.Core;
+
+namespace BookGen.Framework.Scripts
+{
+    internal class PtyhonHost : ProcessHost
+    {
+        public PtyhonHost(ILog log) : base(log)
+        {
+        }
+
+        public override string ProcessFileName => ProcessInterop.AppendExecutableExtension("python");
+
+        public override string ProcessArguments => string.Empty;
+
+        public override string SerializeHostInfo(ScriptHost host)
+        {
+            return JsonInliner.InlinePython(nameof(host), host, _log);
+        }
+    }
+}
