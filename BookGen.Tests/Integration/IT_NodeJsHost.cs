a3ac7c16cfc1ab960003b0b632f64e1bbf7aab36
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2020-01-14 22:13:26 +0100

    Node Hosting works

---
+++ BookGen.Tests/Integration/IT_NodeJsHost.cs
@@ -0,0 +1,57 @@
+﻿using BookGen.Api;
+using BookGen.Core;
+using BookGen.Core.Contracts;
+using BookGen.Domain;
+using BookGen.Framework.Scripts;
+using BookGen.Tests.Environment;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace BookGen.Tests
+{
+    [TestFixture, SingleThreaded]
+    public class IT_NodeJsHost
+    {
+        private NodeJsHost _sut;
+        private Mock<ILog> _log;
+        private Mock<IReadonlyRuntimeSettings> _settings;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _log = new Mock<ILog>();
+            _settings = new Mock<IReadonlyRuntimeSettings>();
+            _settings.Setup(x => x.SourceDirectory).Returns(FsPath.Empty);
+            _sut = new NodeJsHost(_log.Object, _settings.Object);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _sut = null;
+            _settings = null;
+            _log = null;
+        }
+
+        [Test]
+        public void EnsureThat_Generate_RunsSimpleJs()
+        {
+            var result = _sut.Generate(new Arguments(new Dictionary<string, string>
+            {
+                { "file", TestEnvironment.GetFile("SimpleNodeJs.js") }
+            }));
+            Assert.AreEqual("Hello from nodeJS\n", result);
+        }
+
+        [Test]
+        public void EnsureThat_Generate_RunsComplexJs()
+        {
+            var result = _sut.Generate(new Arguments(new Dictionary<string, string>
+            {
+                { "file", TestEnvironment.GetFile("LongNodeJs.js") }
+            }));
+            Assert.AreEqual("0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n", result);
+        }
+    }
+}
--- BookGen/Framework/Scripts/ProcessInterop.cs
+++ BookGen/Framework/Scripts/ProcessInterop.cs
@@ -15,106 +15,34 @@ namespace BookGen.Framework.Scripts
 {
     internal static class ProcessInterop
     {
-        private static Task<bool> WaitForExitAsync(Process process, string stdin, int timeout)
-        {
-            return Task.Run(() =>
-            {
-                process.StandardInput.Write(stdin);
-                return process.WaitForExit(timeout);
-            });
-        }
-
-        public static async Task<(int exitcode, string output)> RunProcess(string command, string arguments, string stdin, int timeout)
+        public static (int exitcode, string output) RunProcess(string command, string arguments, int timeout)
         {
             (int exitcode, string output) result = (-1, string.Empty);
 
             using (var process = new Process())
             {
-                // If you run bash-script on Linux it is possible that ExitCode can be 255.
-                // To fix it you can try to add '#!/bin/bash' header to the script.
-
                 process.StartInfo.FileName = command;
                 process.StartInfo.Arguments = arguments;
                 process.StartInfo.UseShellExecute = false;
-                process.StartInfo.RedirectStandardInput = true;
                 process.StartInfo.RedirectStandardOutput = true;
                 process.StartInfo.RedirectStandardError = true;
                 process.StartInfo.CreateNoWindow = true;
 
-                var outputBuilder = new StringBuilder();
-                var outputCloseEvent = new TaskCompletionSource<bool>();
+                process.Start();
 
-                process.OutputDataReceived += (s, e) =>
+                string output = process.StandardOutput.ReadToEnd();
+                string err = process.StandardError.ReadToEnd();
+
+                if (process.WaitForExit(timeout))
                 {
-                    // The output stream has been closed i.e. the process has terminated
-                    if (e.Data == null)
-                        outputCloseEvent.SetResult(true);
-                    else
-                        outputBuilder.AppendLine(e.Data);
-                };
-
-                var errorBuilder = new StringBuilder();
-                var errorCloseEvent = new TaskCompletionSource<bool>();
-
-                process.ErrorDataReceived += (s, e) =>
-                {
-                    // The error stream has been closed i.e. the process has terminated
-                    if (e.Data == null)
-                        errorCloseEvent.SetResult(true);
-                    else
-                        errorBuilder.AppendLine(e.Data);
-                };
-
-                bool isStarted;
-
-                try
-                {
-                    isStarted = process.Start();
-                }
-                catch (Exception error)
-                {
-                    // Usually it occurs when an executable file is not found or is not executable
-
-                    result.exitcode = -1;
-                    result.output = error.Message;
-                    isStarted = false;
-                }
-
-                if (!isStarted)
-                    return result;
-
-                // Reads the output stream first and then waits because deadlocks are possible
-                process.BeginOutputReadLine();
-                process.BeginErrorReadLine();
-
-                // Creates task to wait for process exit using timeout
-                var waitForExit = WaitForExitAsync(process, stdin, timeout);
-
-                // Create task to wait for process exit and closing all output streams
-                var processTask = Task.WhenAll(waitForExit, outputCloseEvent.Task, errorCloseEvent.Task);
-
-                // Waits process completion and then checks it was not completed by timeout
-                if (await Task.WhenAny(Task.Delay(timeout), processTask) == processTask && waitForExit.Result)
-                {
+                    result.output = output;
                     result.exitcode = process.ExitCode;
-
-                    // Adds process output if it was completed with error
-                    if (process.ExitCode != 0)
-                    {
-                        result.output = $"{outputBuilder}{errorBuilder}";
-                    }
-
-                    return result;
                 }
-
-                try
+                else
                 {
-                    // Kill hung process
+                    result.exitcode = -1;
+                    result.output = err;
                     process.Kill();
-                }
-                catch
-                {
-                    //Intentionally left empty
                 }
             }
             return result;
