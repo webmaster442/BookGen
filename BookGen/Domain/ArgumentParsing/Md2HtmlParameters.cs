2ac5985976162a737379c6d00fc70f761ff2f95a
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2020-01-15 18:35:47 +0100

    Md2HTML integrated into BookGen

---
+++ BookGen/Domain/ArgumentParsing/Md2HtmlParameters.cs
@@ -0,0 +1,23 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2020 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using BookGen.Core;
+
+namespace BookGen.Domain.ArgumentParsing
+{
+    internal class Md2HtmlParameters
+    {
+        public FsPath InputFile { get; }
+        public FsPath OutputFile { get; }
+        public FsPath Css { get; }
+
+        public Md2HtmlParameters(string inputFile, string outputFile, string css)
+        {
+            InputFile = new FsPath(inputFile);
+            OutputFile = new FsPath(outputFile);
+            Css = new FsPath(css);
+        }
+    }
+}
---
+++ BookGen/Mdoules/Md2HtmlModule.cs
@@ -0,0 +1,48 @@
+﻿using BookGen.Api;
+using BookGen.Core;
+using BookGen.Core.Markdown;
+using BookGen.Domain.ArgumentParsing;
+using BookGen.Template;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookGen.Mdoules
+{
+    internal class Md2HtmlModule : ModuleBase
+    {
+        public override string ModuleCommand => "Md2HTML";
+
+        private bool GetMd2HtmlParameters(ArgumentParser arguments, out Md2HtmlParameters md2HtmlParameters)
+        {
+            md2HtmlParameters = new Md2HtmlParameters(
+                arguments.GetSwitchWithValue("-i", "--input"),
+                arguments.GetSwitchWithValue("-o", "--output"),
+                arguments.GetSwitchWithValue("-c", "--css"));
+
+            if (md2HtmlParameters.Css != FsPath.Empty)
+            {
+                return 
+                    md2HtmlParameters.Css.IsExisting
+                    && md2HtmlParameters.InputFile.IsExisting;
+            }
+
+            return
+                md2HtmlParameters.InputFile.IsExisting;
+        }
+
+        public override bool Execute(ArgumentParser tokenizedArguments)
+        {
+            if (!GetMd2HtmlParameters(tokenizedArguments, out Md2HtmlParameters parameters))
+                return false;
+
+            var log = new ConsoleLog(LogLevel.Info);
+
+            string md = parameters.InputFile.ReadFile(log);
+            string rendered = BuiltInTemplates.Print.Replace("[content]", MarkdownRenderers.Markdown2Preview(md, parsed.InputFile.GetDirectory()));
+            parameters.OutputFile.WriteFile(log, rendered);
+
+            return true;
+        }
+    }
+}
