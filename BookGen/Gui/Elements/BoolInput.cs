9c7289a2a94c10b375d7cb832dd38cf71f123d39
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2020-01-22 20:44:38 +0100

    Minimal UI changes

--- BookGen/Gui/Elements/BoolInput.cs
+++ BookGen/Gui/Elements/BoolInput.cs
@@ -1,4 +1,9 @@
-﻿using BookGen.Gui.Renderering;
+﻿//-----------------------------------------------------------------------------
+// (c) 2019 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using BookGen.Gui.Renderering;
 
 namespace BookGen.Gui.Elements
 {
---
+++ BookGen/Gui/TaskMenu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookGen.Gui
+{
+    internal class TaskMenu : ConsoleMenuBase
+    {
+        public override IEnumerable<ConsoleUiElement> CreateElements()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void ProcessInputs()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
