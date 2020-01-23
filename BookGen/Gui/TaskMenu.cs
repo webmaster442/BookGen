3a0b6cf87a6b3e22047cb9cc29f26f15947a7fe3
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2020-01-23 19:48:36 +0100

    Work on Task menu

--- BookGen/Gui/TaskMenu.cs
+++ BookGen/Gui/TaskMenu.cs
@@ -1,18 +1,143 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------------
+// (c) 2019-2020 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using BookGen.Api;
+using BookGen.Core;
+using BookGen.Domain.VsTasks;
+using BookGen.Gui.Elements;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BookGen.Gui
 {
     internal class TaskMenu : ConsoleMenuBase
     {
+        private readonly ILog _log;
+        private readonly FsPath _tasksFile;
+        private readonly bool _isWindows;
+
+        public TaskMenu(ILog log, FsPath WorkDir)
+        {
+            _log = log;
+            _tasksFile = WorkDir.Combine(".vscode\\tasks.json");
+            _isWindows = Environment.OSVersion.Platform == PlatformID.Win32NT;
+        }
+
+        private void StartProcess(string cmd, string args)
+        {
+            try
+            {
+                using (var p = new System.Diagnostics.Process())
+                {
+                    p.StartInfo.FileName = cmd;
+                    p.StartInfo.Arguments = args;
+                    p.StartInfo.UseShellExecute = false;
+                    p.Start();
+                }
+            }
+            catch (Exception ex) when 
+                (ex is InvalidOperationException
+                || ex is System.ComponentModel.Win32Exception)
+            {
+                _log.Warning(ex);
+            }
+        }
+
+        private Button CreateBackPageButton()
+        {
+            return new Button
+            {
+                Action = () => ShouldRun = false,
+                Content = "Back to previous menu"
+            };
+        }
+
+        private Button CreateButtonFromTask(Task task)
+        {
+            return new Button
+            {
+                Content = task?.label ?? "",
+                Action = () =>
+                {
+                    string command = "";
+                    string args = "";
+                    if (_isWindows)
+                    {
+                        command = task?.windows?.command ?? "";
+                        args = task?.windows?.args?.FirstOrDefault()?.value ?? "";
+                        if (task?.type == "shell")
+                        {
+                            command = "cmd.exe /c" + command;
+                        }
+                    }
+                    else
+                    {
+                        command = task?.command ?? "";
+                        args = task?.args?.FirstOrDefault()?.value ?? "";
+                    }
+                    StartProcess(command, args);
+                }
+            };
+        }
+
         public override IEnumerable<ConsoleUiElement> CreateElements()
         {
-            throw new NotImplementedException();
+            VsTaskRoot? tasks = null;
+            if (_tasksFile.IsExisting)
+            {
+                tasks = _tasksFile.DeserializeJson<VsTaskRoot>(_log);
+            }
+
+            if (tasks == null
+                || tasks.tasks == null 
+                || tasks.tasks.Count < 1)
+            {
+                yield return new TextBlock
+                {
+                    Content = "No Tasks file found. Use Initializer to create one\r\n\r\n"
+                };
+                yield return CreateBackPageButton();
+            }
+            else
+            {
+                foreach (var task in tasks.tasks)
+                {
+                    yield return CreateButtonFromTask(task);
+                }
+                yield return CreateBackPageButton();
+            }
         }
 
         protected override void ProcessInputs()
         {
-            throw new NotImplementedException();
+            int? index = Renderer.GetInputChoice();
+
+            Button? actionToDo = null;
+
+            if (index.HasValue)
+            {
+                actionToDo = (from item in Elements
+                              where
+                                 item is Button b
+                                 && b.Entry == index.Value
+                              select
+                                 item as Button).FirstOrDefault();
+            }
+            if (actionToDo == null)
+            {
+                DoRender();
+                Renderer.DisplayError($"\rUnrecognised item: {index}\r\n");
+
+            }
+            else
+            {
+                actionToDo.Action?.Invoke();
+                Renderer.PressKeyContinue();
+                DoRender();
+            }
         }
     }
 }
