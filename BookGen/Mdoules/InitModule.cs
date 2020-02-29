f315973096c57287b08049e78a322126b8fe35d9
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2020-02-29 18:59:00 +0100

    Initialize module rewritten

--- BookGen/Mdoules/InitModule.cs
+++ BookGen/Mdoules/InitModule.cs
@@ -1,8 +1,6 @@
 ﻿using BookGen.ConsoleUi;
 using BookGen.Core;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace BookGen.Mdoules
 {
@@ -27,7 +25,7 @@ namespace BookGen.Mdoules
             var log = new ConsoleLog(Api.LogLevel.Detail);
 
             System.IO.Stream? Ui = typeof(GuiModule).Assembly.GetManifestResourceStream("BookGen.ConsoleUi.InitializeView.xml");
-            var vm = new InitializeViewModel();
+            var vm = new InitializeViewModel(log, new FsPath(dir));
 
             if (Ui != null)
             {
