4fe499d6a5a2bd0890ff83162c9fab911113b34f
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2020-04-18 15:32:10 +0200

    Resources project setup

---
+++ BookGen.Resources/ResourceHandler.cs
@@ -0,0 +1,27 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2019-2020 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using System;
+
+namespace BookGen.Resources
+{
+    public class ResourceHandler
+    {
+        public static string GetFile(string file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string GetFile(KnownFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExtractKnownFile(KnownFile file, string targetDir)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
