4b76c62daa4ddace7a38f5e7d07d80ce4f28879f
webmaster442 <ruzsinszki.gabor@gmail.com>
2020-05-03 12:59:32 +0200

    Config documented for new version

--- BookGen.Core/Configuration/ImageOptions.cs
+++ BookGen.Core/Configuration/ImageOptions.cs
@@ -9,25 +9,25 @@ namespace BookGen.Core.Configuration
 {
     public sealed class ImageOptions : IReadonlyImageOptions
     {
-        [Doc("If set to true, then JPEG images will be converted to WebP format")]
+        [Doc("If set to true, then JPEG images will be converted to WebP format", IsOptional = true)]
         public bool RecodeJpegToWebp { get; set; }
 
-        [Doc("If set to true, then output images will be resized")]
+        [Doc("If set to true, then output images will be resized", IsOptional = true)]
         public bool EnableResize { get; set; }
 
-        [Doc("Maximal output image width when resize enabled")]
+        [Doc("Maximal output image width when resize enabled", IsOptional = true)]
         public int MaxWidth { get; set; }
 
-        [Doc("Maximal output image height when resize enabled")]
+        [Doc("Maximal output image height when resize enabled", IsOptional = true)]
         public int MaxHeight { get; set; }
 
-        [Doc("If set to true, then SVG files will be converted to PNG. Render size is set by the MaxWidth and MaxHeight property")]
+        [Doc("If set to true, then SVG files will be converted to PNG. Render size is set by the MaxWidth and MaxHeight property", IsOptional = true)]
         public bool RenderSvgToPng { get; set; }
 
-        [Doc("Inline images, that are smaller the given limit in bytes")]
+        [Doc("Inline images, that are smaller the given limit in bytes", IsOptional = true)]
         public long InlineImageSizeLimit { get; set; }
 
-        [Doc("WebP codec quality. Minimum 0 and maximum 100. Only used, when RecodeJpegToWebp enabled.")]
+        [Doc("WebP codec quality. Minimum 0 and maximum 100. Only used, when RecodeJpegToWebp enabled.", IsOptional = true)]
         public int WebPQuality { get; set; }
 
         public ImageOptions()
