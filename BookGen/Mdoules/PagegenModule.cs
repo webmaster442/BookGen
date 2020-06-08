09a91c43696e4c345409d472e6f0e8a4431392d3
webmaster442 <ruzsinszki.gabor@gmail.com>
2020-06-08 10:13:25 +0200

    PageGen module skeleton

---
+++ BookGen/Mdoules/PagegenModule.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2020 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using BookGen.Core;
+using BookGen.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookGen.Mdoules
+{
+    internal class PagegenModule : ModuleBase
+    {
+        public PagegenModule(ProgramState currentState) : base(currentState)
+        {
+        }
+
+        public override string ModuleCommand => "PageGen";
+
+        public override bool Execute(ArgumentParser tokenizedArguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetHelp()
+        {
+            return HelpUtils.GetHelpForModule(nameof(PagegenModule));
+        }
+    }
+}
