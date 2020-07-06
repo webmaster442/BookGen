cf67e6c184db9b85c5821f2c763672dc7a5c2eed
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2020-07-06 19:39:22 +0200

    Autocompleter installer

---
+++ BookGen/Domain/ArgumentParsing/InstallPsArguments.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2020 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using BookGen.Ui.ArgumentParser;
+
+namespace BookGen.Domain.ArgumentParsing
+{
+    internal class InstallPsArguments : ArgumentsBase
+    {
+        [Switch("-dn", "--dotnet")]
+        public bool Dotnet { get; set; }
+
+        public override bool Validate()
+        {
+            return Files.Length == 1;
+        }
+    }
+}
---
+++ BookGen/Modules/InstallPSAutocompleteModule.cs
@@ -0,0 +1,60 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2020 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using BookGen.Api;
+using BookGen.Core;
+using BookGen.Domain.ArgumentParsing;
+using BookGen.Domain.Shell;
+using BookGen.Resources;
+using BookGen.Ui.ArgumentParser;
+using BookGen.Utilities;
+using System.Text;
+
+namespace BookGen.Modules
+{
+    internal class InstallPSAutocompleteModule : StateModuleBase
+    {
+        protected InstallPSAutocompleteModule(ProgramState currentState) : base(currentState)
+        {
+        }
+
+        public override AutoCompleteItem AutoCompleteInfo => new AutoCompleteItem(ModuleCommand);
+
+        public override string ModuleCommand => "InstallPsAutocomplete";
+
+        public override bool Execute(string[] arguments)
+        {
+            var args = new InstallPsArguments();
+            if (!ArgumentParser.ParseArguments(arguments, args))
+            {
+                return false;
+            }
+
+
+            ILog log = new ConsoleLog(LogLevel.Info);
+            FsPath target = new FsPath(args.Files[0]);
+
+            StringBuilder contents = new StringBuilder(4096);
+
+            if (args.Dotnet)
+            {
+                var dnc = ResourceHandler.GetResourceFile<GeneratorRunner>("Resources/completer.dn.ps1");
+                contents.Append(dnc);
+            }
+
+            var completer = ResourceHandler.GetResourceFile<GeneratorRunner>("Resources/completer.ps1");
+            contents.Append(completer);
+
+            target.WriteFile(log, contents.ToString());
+
+            return true;
+        }
+
+        public override string GetHelp()
+        {
+            return HelpUtils.GetHelpForModule(nameof(InstallPSAutocompleteModule));
+        }
+    }
+}
