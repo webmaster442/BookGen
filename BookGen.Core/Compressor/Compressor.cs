94e9487d79de48e1c458daa131b78fc3eb9254a0
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-04-18 15:14:31 +0200

    HTML compressor

---
+++ BookGen.Core/Compressor/Compressor.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2021 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using ZetaProducerHtmlCompressor.Internal;
+
+namespace BookGen.Core.Compressor
+{
+    public static class Compressor
+    {
+        public static string CompressHtml(string source)
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                return string.Empty;
+            }
+
+            HtmlCompressor compressor = new HtmlCompressor();
+            compressor.setRemoveComments(true);
+            compressor.setCssCompressor(new CssCompressor());
+            compressor.setCompressCss(true);
+            compressor.setJavaScriptCompressor(new JsCompressor());
+            compressor.setCompressJavaScript(true);
+
+            return compressor.compress(source);
+        }
+    }
+}
---
+++ BookGen.Core/Compressor/JsCompressor.cs
@@ -0,0 +1,18 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2021 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using NUglify;
+
+namespace BookGen.Core.Compressor
+{
+    internal class JsCompressor : CompressorBase
+    {
+        public override string Compress(string source)
+        {
+            UglifyResult ugly = Uglify.Js(source);
+            return ProcessCompress(ugly);
+        }
+    }
+}
