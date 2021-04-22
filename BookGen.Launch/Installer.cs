05fed37cea7461038c5a712005d6d86f892a9284
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-04-22 19:54:19 +0200

    Work on Launcher installer

--- BookGen.Launch/Installer.cs
+++ BookGen.Launch/Installer.cs
@@ -3,36 +3,20 @@
 // This code is licensed under MIT license (see LICENSE for details)
 //-----------------------------------------------------------------------------
 
-using Microsoft.Win32;
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Windows.Forms;
 
 namespace BookGen.Launch
 {
     internal class Installer
     {
-        public bool IsInstalled
-        {
-            get
-            {
-                RegistryKey? key  = Registry.CurrentUser.OpenSubKey("Software", true);
-                var subkey = key?.OpenSubKey(nameof(BookGen.Launch));
-                if (subkey?.GetValue("isInstalled", false) is bool value)
-                {
-                    return value;
-                }
-                return false;
-            }
-            set
-            {
-                RegistryKey? key = Registry.CurrentUser.OpenSubKey("Software", true);
-                var subkey = key?.OpenSubKey(nameof(BookGen.Launch));
-                subkey?.SetValue("isInstalled", value);
-
-            }
-        }
+        private readonly static string _Directory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        private const int STGM_READ = 0x00000000;
 
         private static void CreateLink(string PathToFile,
                           string PathToLink,
@@ -50,17 +34,58 @@ namespace BookGen.Launch
             }
         }
 
-        public void CreateShortcut()
+        public static bool IsInstalled
         {
-            string dir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            string linkFileName = Path.Combine(dir, "BookGen.Launch" + ".lnk");
-            //CreateLink(program, linkFileName, string.Empty, string.Empty);
+            get
+            {
+                bool installed = false;
+                foreach (var linkFile in Directory.GetFiles(_Directory, "*.lnk"))
+                {
+                    if (installed)
+                    {
+                        break;
+                    }
+                    if (new ShellLink() is IShellLinkW shellLink)
+                    {
+                        ((IPersistFile)shellLink).Load(linkFile, STGM_READ);
+
+                        StringBuilder buffer = new StringBuilder(4096);
+                        shellLink.GetPath(buffer, buffer.Capacity, IntPtr.Zero, 0);
+
+                        var file = buffer.ToString();
+
+                        if (file == Process.GetCurrentProcess().StartInfo.FileName)
+                        {
+                            installed = true;
+                        }
+                        Marshal.ReleaseComObject(shellLink);
+                    }
+                }
+                return installed;
+            }
         }
 
-
-        public Installer()
+        private static void CreateShortcut()
         {
+            var program = Process.GetCurrentProcess().StartInfo.FileName;
 
+            string linkFileName = Path.Combine(_Directory, "Launch BookGen Shell" + ".lnk");
+            CreateLink(program, linkFileName, string.Empty, string.Empty);
+        }
+
+        public static void HandleInstall()
+        {
+            if (!IsInstalled)
+            {
+                var result = MessageBox.Show("Would you like to create a shortcut on the desktop?",
+                                             "Create Shortcut",
+                                             MessageBoxButtons.YesNo,
+                                             MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    CreateShortcut();
+                }
+            }
         }
     }
 }
