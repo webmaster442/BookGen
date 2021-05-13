8b1c567fae234180a4fdf09e2145be7bc6eb6660
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-05-13 16:31:38 +0200

    Help view

--- BookGen.Gui/Mvvm/ViewModelBase.cs
+++ BookGen.Gui/Mvvm/ViewModelBase.cs
@@ -3,11 +3,20 @@
 // This code is licensed under MIT license (see LICENSE for details)
 //-----------------------------------------------------------------------------
 
+using System.ComponentModel;
+
 namespace BookGen.Ui.Mvvm
 {
-    public abstract class ViewModelBase
+    public abstract class ViewModelBase : INotifyPropertyChanged
     {
         public IView? View { get; private set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void Notify(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
 
         public void InjectView(IView view)
         {
---
+++ BookGen/ConsoleUi/HelpViewModel.cs
@@ -0,0 +1,44 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2021 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using BookGen.Ui.Mvvm;
+using BookGen.Utilities;
+using System.Collections.Generic;
+
+namespace BookGen.ConsoleUi
+{
+    public class HelpViewModel : ViewModelBase
+    {
+        private int _selectedIndex;
+
+        public List<string> AvailableCommands { get; }
+
+        public int SelectedIndex
+        {
+            get { return _selectedIndex; }
+            set
+            {
+                _selectedIndex = value;
+                UpdateText(value);
+                Notify(nameof(CommandText));
+            }
+        }
+
+        public string CommandText { get; set; }
+
+        public HelpViewModel(IEnumerable<string> commands)
+        {
+            CommandText = string.Empty;
+            AvailableCommands = new List<string>(commands);
+            SelectedIndex = 0;
+        }
+
+        private void UpdateText(int value)
+        {
+            var moduleName = AvailableCommands[value];
+            CommandText = HelpUtils.GetHelpForModule(moduleName);
+        }
+    }
+}
