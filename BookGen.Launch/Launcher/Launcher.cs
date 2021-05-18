65a292b201db77a5656cc208362393b4d41258b7
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-05-18 20:24:22 +0200

    Ported usefull code from existing project

---
+++ BookGen.Launch/Launcher/Launcher.cs
@@ -0,0 +1,96 @@
+﻿using BookGen.Resources;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace BookGen.Launch.Launcher
+{
+    internal class Launcher
+    {
+        private readonly string _appdir;
+        private string _selectedFolder;
+
+        internal Launcher()
+        {
+            _appdir = AppContext.BaseDirectory;
+            _selectedFolder = string.Empty;
+        }
+
+        private static void Message(string text, MessageBoxImage icon)
+        {
+            MessageBox.Show(text, icon.ToString(), MessageBoxButton.OK, icon);
+        }
+
+        internal void Run(bool useWinTerminal)
+        {
+            if (!TryselectFolder(out _selectedFolder))
+            {
+                Message("No folder was selected. Application will exit.", MessageBoxImage.Information);
+                Environment.Exit(1);
+            }
+
+            string _shellScript;
+            if (!TryCreateShellScript(out _shellScript))
+            {
+                Message("Failed to write start script. Application will exit.", MessageBoxImage.Error);
+                Environment.Exit(2);
+            }
+
+            if (!TerminalLauncher.Launch(_shellScript, useWinTerminal))
+            {
+                Message("Failed to start shell. Application will exit.", MessageBoxImage.Error);
+                Environment.Exit(3);
+            }
+        }
+
+        private static bool TryselectFolder(out string folderPath)
+        {
+            using var dialog = new System.Windows.Forms.FolderBrowserDialog
+            {
+                Description = "Select a folder to start bookgen shell",
+                UseDescriptionForTitle = true,
+                ShowNewFolderButton = false,
+                AutoUpgradeEnabled = true
+            };
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                folderPath = dialog.SelectedPath;
+                return true;
+            }
+            else
+            {
+                folderPath = string.Empty;
+                return false;
+            }
+        }
+
+        private bool TryCreateShellScript(out string shellScriptPath)
+        {
+            try
+            {
+                var name = Path.GetTempFileName();
+                name = Path.ChangeExtension(name, ".ps1");
+
+                using (var file = File.CreateText(name))
+                {
+                    var dn = ResourceHandler.GetResourceFile<KnownFile>("Powershell/completer.dn.ps1");
+                    var bg = ResourceHandler.GetResourceFile<KnownFile>("Powershell/completer.ps1");
+
+                    file.WriteLine("$env:Path += \";{0}\"", _appdir);
+                    file.WriteLine("Set-Location \"{0}\"", _selectedFolder);
+                    file.WriteLine(dn);
+                    file.WriteLine(bg);
+                    file.WriteLine("Remove-Item \"{0}\"", name);
+                }
+
+                shellScriptPath = name;
+                return true;
+            }
+            catch (IOException)
+            {
+                shellScriptPath = string.Empty;
+                return false;
+            }
+        }
+    }
+}
---
+++ BookGen.Launch/Launcher/RegistryAdapter.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+
+namespace BookGen.Launch.Launcher
+{
+    internal class RegistryAdapter
+    {
+        private const int maxDirectories = 10;
+        private readonly string _subKey;
+
+        public RegistryAdapter(string subKey)
+        {
+            _subKey = subKey;
+        }
+
+        public IEnumerable<string> GetRecentDirectoryList()
+        {
+            using var key = Registry.CurrentUser.OpenSubKey(_subKey);
+            if (key != null)
+            {
+                for (int i = 0; i < maxDirectories; i++)
+                {
+                    var dirKey = key.GetValue($"Dir{i}");
+                    if (dirKey != null)
+                    {
+                        yield return (string)dirKey;
+                    }
+                    else
+                    {
+                        yield break;
+                    }
+                }
+            }
+        }
+
+        public void SaveRecentDirectoryList(IList<string> recentFolders)
+        {
+            using var key = Registry.CurrentUser.CreateSubKey(_subKey);
+            if (key != null)
+            {
+                int count = Math.Min(recentFolders.Count, maxDirectories);
+                for (int i=0; i<count; i++)
+                {
+                    key.SetValue($"Dir{i}", recentFolders[i], RegistryValueKind.String);
+                }
+            }
+        }
+
+        public void DeleteRecentDirectoryList()
+        {
+            using var key = Registry.CurrentUser.CreateSubKey(_subKey);
+            if (key != null)
+            {
+                for (int i=0; i<maxDirectories; i++)
+                {
+                    var dirKey = key.GetValue($"Dir{i}");
+                    if (dirKey != null)
+                    {
+                        key.DeleteValue($"Dir{i}");
+                    }
+                }
+            }
+        }
+    }
+}
