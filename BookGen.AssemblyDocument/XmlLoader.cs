eacfc713a056c64ecbb60eb9c9a816e395596af1
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-05-26 19:22:19 +0200

    XSD validated loading

---
+++ BookGen.AssemblyDocument/XmlLoader.cs
@@ -0,0 +1,43 @@
+﻿using BookGen.AssemblyDocument.XmlDoc;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace BookGen.AssemblyDocument
+{
+    public class XmlLoader
+    {
+        private readonly XmlSchemaSet _validators;
+
+        public XmlLoader()
+        {
+            var xsd = Resources.ResourceHandler.GetResourceFile<XmlLoader>("Schema.xsd");
+            _validators = new XmlSchemaSet();
+            _validators.Add("", XmlReader.Create(new StringReader(xsd)));
+        }
+
+        public bool TryValidatedLoad(string file, out Doc? document)
+        {
+            XDocument doc = XDocument.Load(file);
+            document = null;
+            bool valid = true;
+            doc.Validate(_validators, (o, e) =>
+            {
+                valid = false;
+            });
+
+            if (valid)
+            {
+                XmlSerializer xs = new XmlSerializer(typeof(Doc));
+                using (var f = File.OpenRead(file))
+                {
+                    document = xs.Deserialize(f) as Doc;
+                }
+            }
+
+            return valid;
+        }
+    }
+}
