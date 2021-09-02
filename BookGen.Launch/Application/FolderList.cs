a424e1ea755b2ff29d79a831df09286188bbac22
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-09-02 19:14:44 +0200

    Settings and small restructure

---
+++ BookGen.Launch/Application/FolderList.cs
@@ -0,0 +1,29 @@
+﻿using BookGen.Launch.Properties;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace BookGen.Launch.Application
+{
+    internal static class FolderList
+    {
+        public static IList<string> GetFolders()
+        {
+            if (string.IsNullOrEmpty(Settings.Default.FolderListJson))
+                return new List<string>();
+
+            string[]? deserialized = JsonSerializer.Deserialize<string[]>(Settings.Default.FolderListJson);
+            if (deserialized != null)
+                return deserialized;
+
+            return new List<string>();
+        }
+
+        public static void SaveFolders(IList<string> folders)
+        {
+            var text = JsonSerializer.Serialize(folders);
+            Settings.Default.FolderListJson = text;
+            Settings.Default.Save();
+        }
+
+    }
+}
