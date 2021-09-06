3e8842e45d3c1c560dc7337d4ee0289fedff196b
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-09-06 20:42:01 +0200

    Update script

--- BookGen.Resources/Powershell/updater.ps1
+++ BookGen.Resources/Powershell/updater.ps1
@@ -6,17 +6,29 @@ if ($($args.Count) -lt 2) {
 	exit
 }
 
+clear
+Write-host "  ____              _     _____              _    _           _       _            "
+Write-host " |  _ \            | |   / ____|            | |  | |         | |     | |           "
+Write-host " | |_) | ___   ___ | | _| |  __  ___ _ __   | |  | |_ __   __| | __ _| |_ ___ _ __ "
+Write-host " |  _ < / _ \ / _ \| |/ / | |_ |/ _ \ '_ \  | |  | | '_ \ / _| |/ _| | __/ _ \ '__|"
+Write-host " | |_) | (_) | (_) |   <| |__| |  __/ | | | | |__| | |_) | (_| | (_| | ||  __/ |   "
+Write-host " |____/ \___/ \___/|_|\_\\_____|\___|_| |_|  \____/| .__/ \__,_|\__,_|\__\___|_|   "
+Write-host "                                                   | |                             "
+Write-host "                                                   |_|                             "
+
+
 Write-host Stopping all running BookGen instances...
 if((get-process "BookGen" -ea SilentlyContinue) -ne $Null){ 
 	Stop-process -name BookGen -Force
 }
 
 Write-host Downloading update package...
-Invoke-WebRequest -Uri $1 -OutFile $env:temp\update.zip
+Invoke-WebRequest -Uri $file -OutFile $env:temp\update.zip
 
 Write-host Verifying integrity...
 if ($hash -ne (Get-FileHash -Algorithm SHA256 $env:temp\update.zip).Hash) {
 	Write-host "Package integrity check failed"
+	Remove-Item -Path $env:temp\update.zip -Force
 	exit
 }
 
@@ -28,4 +40,7 @@ Remove-Item -Path $env:temp\update.zip -Force
 
 Write-host DONE!
 Write-host Press a key to exit
+
+Remove-Item -Path .\updater.ps1
+
 Read-Host
