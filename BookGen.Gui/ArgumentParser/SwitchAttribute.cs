dd9345b3db9cf7034f492a05737c92567a2f5049
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-09-15 17:50:51 +0200

    Added missing license headers

--- BookGen.Gui/ArgumentParser/SwitchAttribute.cs
+++ BookGen.Gui/ArgumentParser/SwitchAttribute.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------------
+// (c) 2020-2021 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 
 namespace BookGen.Gui.ArgumentParser
