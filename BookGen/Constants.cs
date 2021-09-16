307f17be91960378fb39b1c7bc34a55fe42cef2f
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-09-16 22:32:54 +0200

    Wiki Module

---
+++ BookGen/Constants.cs
@@ -0,0 +1,12 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2021 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+namespace BookGen
+{
+    internal static class Constants
+    {
+        public const string WikiUrl = "https://github.com/webmaster442/BookGen/wiki";
+    }
+}
---
+++ BookGen/Utilities/UrlOpener.cs
@@ -0,0 +1,30 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2021 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using System;
+
+namespace BookGen.Utilities
+{
+    internal static class UrlOpener
+    {
+        internal static bool OpenUrl(string url)
+        {
+            try
+            {
+                using (var process = new System.Diagnostics.Process())
+                {
+                    process.StartInfo.UseShellExecute = true;
+                    process.StartInfo.FileName = url;
+                    process.Start();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
