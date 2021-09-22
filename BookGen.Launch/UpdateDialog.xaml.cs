000900fe9c4780a249a53c3c31c231573bd176d4
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-09-22 18:45:55 +0200

    Preliminary version of update dialog

---
+++ BookGen.Launch/UpdateDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace BookGen.Launch
+{
+    /// <summary>
+    /// Interaction logic for UpdateDialog.xaml
+    /// </summary>
+    public partial class UpdateDialog : Window
+    {
+        public UpdateDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
