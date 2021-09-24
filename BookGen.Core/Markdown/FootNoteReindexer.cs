284ad403dd458767efde1a1dd411faa6a925ba49
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-09-24 21:18:08 +0200

    FootNote reindexing component

---
+++ BookGen.Core/Markdown/FootNoteReindexer.cs
@@ -0,0 +1,89 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2021 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BookGen.Core.Markdown
+{
+    public class FootNoteReindexer
+    {
+        private readonly StringBuilder _footnotes;
+        private readonly StringBuilder _regulartext;
+        private readonly static Regex footnoteRef = new Regex(@"\[\^\d+\]");
+        private readonly static Regex footnoteDef = new Regex(@"\[\d+\]:");
+        private int _counter;
+
+
+        public FootNoteReindexer()
+        {
+            _regulartext = new StringBuilder(8096);
+            _footnotes = new StringBuilder(4096);
+            _counter = 0;
+        }
+
+        public void Clear()
+        {
+            _footnotes.Clear();
+            _regulartext.Clear();
+            _counter = 0;
+        }
+
+        public void AddMarkdown(string document)
+        {
+            int currentDocLimit = 0;
+            var referenceMatches = footnoteRef.Matches(document);
+            var lastDefinition = footnoteRef.Matches(document).LastOrDefault();
+
+            if (referenceMatches.Count == 0 && lastDefinition == null)
+            {
+                //document doesn't contain footnotes
+                _regulartext.Append(document);
+                return;
+            }
+
+            if (lastDefinition != null)
+            {
+                var numberString = lastDefinition.Value[2..^1];
+                currentDocLimit = int.Parse(numberString);
+
+                if (currentDocLimit != referenceMatches.Count)
+                {
+                    //Log this
+                    return;
+                }
+            }
+
+            var definitionStart = footnoteDef.Matches(document).FirstOrDefault();
+            if (definitionStart != null)
+            {
+                var regular = new StringBuilder(document.Substring(0, definitionStart.Index));
+                var footnote = new StringBuilder(document.Substring(definitionStart.Index));
+                DoReindexing(currentDocLimit, regular, footnote);
+                _regulartext.Append(regular);
+                _footnotes.Append(footnote);
+                _counter += currentDocLimit;
+            }
+        }
+
+        private void DoReindexing(int currentDocLimit, StringBuilder regular, StringBuilder footnote)
+        {
+            for (int i = 0; i < currentDocLimit; i++)
+            {
+                int indexToReplace = i + 1;
+                int targetIndex = _counter + i + 1;
+
+                regular.Replace($"[^{indexToReplace}]", $"[^{targetIndex}]");
+                footnote.Replace($"[{indexToReplace}]:", $"[{targetIndex}]:");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{_regulartext}{_footnotes}";
+        }
+    }
+}
---
+++ BookGen.Tests/UT_FootNoteReindexer.cs
@@ -0,0 +1,96 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2021 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using BookGen.Core.Markdown;
+using NUnit.Framework;
+
+namespace BookGen.Tests
+{
+    public class UT_FootNoteReindexer
+    {
+        private FootNoteReindexer _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new FootNoteReindexer();
+        }
+
+        [Test]
+        public void EnsureThat_FootNoteReindexer_ReindexingWorks()
+        {
+            string s1 =
+                  "first[^1]\r\n"
+                + "second[^2]\r\n"
+                + "\r\n"
+                + "[1]: first\r\n"
+                + "[2]: second\r\n";
+
+            string s2 =
+                "third[^1]\r\n"
+                + "fourth[^2]\r\n"
+                + "\r\n"
+                + "[1]: third\r\n"
+                + "[2]: forth\r\n";
+
+            string expected =
+                "first[^1]\r\n"
+                + "second[^2]\r\n"
+                + "\r\n"
+                + "third[^3]\r\n"
+                + "fourth[^4]\r\n"
+                + "\r\n"
+                + "[1]: first\r\n"
+                + "[2]: second\r\n"
+                + "[3]: third\r\n"
+                + "[4]: forth\r\n";
+            _sut.AddMarkdown(s1);
+            _sut.AddMarkdown(s2);
+
+            string result = _sut.ToString();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void EnsureThat_FootNoteReindexer_ReindexingWorksComplex()
+        {
+            string s1 =
+                "first[^1]\r\n"
+                + "second[^2]\r\n"
+                + "\r\n"
+                + "[1]: first\r\n"
+                + "[2]: second\r\n";
+
+            string s2 =
+                "third[^1]\r\n"
+                + "fourth[^2]\r\n"
+                + "\r\n"
+                + "[1]: third\r\n"
+                + "[2]: forth\r\n";
+
+            string expected =
+                "first[^1]\r\n"
+                + "second[^2]\r\n"
+                + "\r\n"
+                + "foo\r\n"
+                + "third[^3]\r\n"
+                + "fourth[^4]\r\n"
+                + "\r\n"
+                + "[1]: first\r\n"
+                + "[2]: second\r\n"
+                + "[3]: third\r\n"
+                + "[4]: forth\r\n";
+            _sut.AddMarkdown(s1);
+            _sut.AddMarkdown("foo\r\n");
+            _sut.AddMarkdown(s2);
+
+            string result = _sut.ToString();
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
