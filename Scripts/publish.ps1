b40b1cfc1263f0d252d7a88af9e0a10db303c5bd
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-11-07 19:39:29 +0100

    Publish script updated

--- Scripts/publish.ps1
+++ Scripts/publish.ps1
@@ -12,6 +12,8 @@ Write-Host "Creating changelog..."
 cd bin\Publish\ 
 .\BookGen.exe Md2HTML -i ..\..\Changelog.md -ns -o ChangeLog.html
 .\BookGen.exe Md2HTML -i ..\..\notes.md -ns -o RelaseNotes.html
+$version = (.\BookGen.exe version -bd) | Out-String
+$version = $version -replace "`t|`n|`r",""
 cd ..
 cd ..
 
@@ -25,6 +27,13 @@ Compress-Archive @compress
 
 Write-Host "Creating hash..."
 Get-FileHash -Path bin\published.zip -Algorithm SHA256 > bin\published.txt
+
+Write-Host "Creating installer..."
+cd BookGen.Setup
+Write-Output "#define MyAppVersion ""$version""" | Out-File -FilePath "version.iss" -Encoding ASCII
+& 'C:\Program Files (x86)\Inno Setup 6\ISCC.exe' setup.iss
+cd ..
+
 
 Write-Host "Cleanup..."
 Remove-Item bin\Release\ -Recurse
