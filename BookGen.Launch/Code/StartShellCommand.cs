074712ae0b5c3b17de9d7775c01d2f86c96ff249
webmaster442 <ruzsinszki.gabor@gmail.com>
2022-03-21 19:30:32 +0100

    Shell script extracted & Icon copy to output Better Terminal support progress

--- BookGen.Launch/Code/StartShellCommand.cs
+++ BookGen.Launch/Code/StartShellCommand.cs
@@ -1,9 +1,8 @@
 ﻿//-----------------------------------------------------------------------------
-// (c) 2021 Ruzsinszki Gábor
+// (c) 2021-2022 Ruzsinszki Gábor
 // This code is licensed under MIT license (see LICENSE for details)
 //-----------------------------------------------------------------------------
 
-using BookGen.Resources;
 using System;
 using System.IO;
 using System.Windows;
@@ -12,39 +11,6 @@ namespace BookGen.Launch.Code
 {
     internal sealed class StartShellCommand : ProcessCommandBase
     {
-        private bool TryCreateShellScript(string folder, out string shellScriptPath)
-        {
-            try
-            {
-#pragma warning disable S5445 // Insecure temporary file creation methods should not be used
-                string? name = Path.GetTempFileName();
-#pragma warning restore S5445 // Insecure temporary file creation methods should not be used
-                name = Path.ChangeExtension(name, ".ps1");
-
-                using (var file = File.CreateText(name))
-                {
-                    var dn = ResourceHandler.GetResourceFile<KnownFile>("Powershell/completer.dn.ps1");
-                    var bg = ResourceHandler.GetResourceFile<KnownFile>("Powershell/completer.ps1");
-                    var sh = ResourceHandler.GetResourceFile<KnownFile>("Powershell/shell.ps1");
-
-                    file.WriteLine("$env:Path += \";{0}\"", AppContext.BaseDirectory);
-                    file.WriteLine("Set-Location \"{0}\"", folder);
-                    file.WriteLine(dn);
-                    file.WriteLine(bg);
-                    file.WriteLine("Remove-Item \"{0}\"", name);
-                    file.WriteLine(sh);
-                }
-
-                shellScriptPath = name;
-                return true;
-            }
-            catch (IOException)
-            {
-                shellScriptPath = string.Empty;
-                return false;
-            }
-        }
-
         private bool Launch(string shellScript)
         {
             if (isWindowsTerminalInstalled && Properties.Settings.Default.UseWindowsTerminal)
@@ -70,14 +36,14 @@ namespace BookGen.Launch.Code
                 return;
             }
 
-            string _shellScript;
-            if (!TryCreateShellScript(folder, out _shellScript))
+            string shellScript = Path.Combine(AppContext.BaseDirectory, "BookGenShell.ps1");
+            if (!File.Exists(shellScript))
             {
                 Message(Properties.Resources.ShellScriptWriteFail, MessageBoxImage.Error);
                 return;
             }
 
-            if (!Launch(_shellScript))
+            if (!Launch(shellScript))
             {
                 Message(Properties.Resources.ShellScriptStartFail, MessageBoxImage.Error);
             }
---
+++ BookGenShell.ps1
@@ -0,0 +1,47 @@
+# -----------------------------------------------------------------------------
+# BookGen PowerShell Registration script
+# Version 2.0
+# Last modified: 2022-03-19
+# -----------------------------------------------------------------------------
+
+# register scripts folder to the path
+$env:Path += ";$PSScriptRoot"
+
+# PowerShell parameter completion shim for BookGen
+Register-ArgumentCompleter -Native -CommandName BookGen -ScriptBlock {
+	param($commandName, $wordToComplete, $cursorPosition)
+		BookGen.exe "Shell" "$wordToComplete" | ForEach-Object {
+			[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+	 }
+}
+# Case invariant registration
+Register-ArgumentCompleter -Native -CommandName bookgen -ScriptBlock {
+	param($commandName, $wordToComplete, $cursorPosition)
+		BookGen.exe "Shell" "$wordToComplete" | ForEach-Object {
+			[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+	 }
+}
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+	param($commandName, $wordToComplete, $cursorPosition)
+		dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+			[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+	 }
+}
+
+# set prompt
+function prompt {
+    $git = $(BookGen.ShellHelper.exe "prompt" $(Get-Location).Path) | Out-String
+    'PS ' +  $(Get-Location) + ' '+$git+ $(if ($NestedPromptLevel -ge 1) { '>>' }) + ' > '
+}
+
+
+# if argument given set to startup folder
+if ($args.Count -eq 1) {
+	Set-Location $args[0]
+}
+
+# welcome message
+Write-Host "To get info on using bookgen type: Bookgen Help"
+Write-Host "To get list of commands type: Bookgen SubCommands"
