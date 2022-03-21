205e9dad393cccd96ac6411d3e3cf455d2ee34c6
webmaster442 <ruzsinszki.gabor@gmail.com>
2022-03-21 20:03:00 +0100

    Better windows terminal support: Profile fragment creation stub

---
+++ BookGen.ShellHelper/Domain/WindowsTerminalProfile.cs
@@ -0,0 +1,39 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2022 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using System.Text.Json.Serialization;
+
+namespace BookGen.ShellHelper.Domain
+{
+    public class WindowsTerminalProfile
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; init; }
+
+        [JsonPropertyName("commandline")]
+        public string CommandLine { get; init; }
+
+        [JsonPropertyName("startingDirectory")]
+        public string StartingDirectory { get; init; }
+
+        [JsonPropertyName("icon")]
+        public string Icon { get; init; }
+
+        [JsonPropertyName("tabTitle")]
+        public string TabTitle { get; init; }
+
+        [JsonPropertyName("hidden")]
+        public bool Hidden { get; init; }
+
+        public WindowsTerminalProfile()
+        {
+            Name = string.Empty;
+            CommandLine = string.Empty;
+            StartingDirectory = string.Empty;
+            Icon = string.Empty;
+            TabTitle = string.Empty;
+        }
+    }
+}
---
+++ BookGen.ShellHelper/TerminalProfileInstaller.cs
@@ -0,0 +1,42 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2022 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using BookGen.ShellHelper.Domain;
+using System;
+using System.IO;
+
+namespace BookGen.ShellHelper
+{
+    public static class TerminalProfileInstaller
+    {
+        public static void Install()
+        {
+            string title = "BookGen Shell";
+#if DEBUG
+            title = "BookGen Shell (Dev version)";
+#endif
+            var profile = CreateProfile(title);
+        }
+
+        private static WindowsTerminalProfile CreateProfile(string title)
+        {
+            return new WindowsTerminalProfile
+            {
+                StartingDirectory = "%userprofile%",
+                Hidden = false,
+                Icon = Path.Combine(AppContext.BaseDirectory, "bookgen-icon.png"),
+                Name = title,
+                TabTitle = title,
+                CommandLine = GetCommandLine(),
+            };
+        }
+
+        private static string GetCommandLine()
+        {
+            string shellScript = Path.Combine(AppContext.BaseDirectory, "BookGenShell.ps1");
+            return $"powershell.exe -ExecutionPolicy Bypass -NoExit -File \"{shellScript}\"";
+        }
+    }
+}
