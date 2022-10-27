b34d6c251f12dc082181d8c9da0f196455bd385f
webmaster442 <ruzsinszki.gabor@gmail.com>
2022-10-27 21:01:17 +0200

    Rake algorithm for keyword extraction

---
+++ Libs/BookGen.RakeEngine/Internals/StringSplitter.cs
@@ -0,0 +1,42 @@
+﻿namespace BookGen.RakeEngine.Internals
+{
+    internal ref struct StringSplitter
+    {
+        private readonly ReadOnlySpan<char> _text;
+        private readonly char _seperator;
+        private int _position;
+
+        public StringSplitter(ReadOnlySpan<char> text, char seperator)
+        {
+            _text = text;
+            _seperator = seperator;
+            _position = 0;
+        }
+
+        public bool TryGetNext(out ReadOnlySpan<char> result)
+        {
+            if (_position >= _text.Length)
+            {
+                result = default;
+                return false;
+            }
+
+            int start = _position;
+            int commaIndex = _text[_position..].IndexOf(_seperator);
+
+            if (commaIndex > -1)
+            {
+                _position += commaIndex + 1;
+                result = _text.Slice(start, commaIndex);
+            }
+            else
+            {
+                _position = _text.Length;
+                result = _text[start..];
+            }
+
+            return true;
+
+        }
+    }
+}
---
+++ Libs/BookGen.RakeEngine/Rake.cs
@@ -0,0 +1,90 @@
+﻿using BookGen.RakeEngine.Internals;
+using System.Globalization;
+using System.Text;
+
+namespace BookGen.RakeEngine
+{
+    public sealed class Rake
+    {
+        private readonly int _minCharLength;
+        private readonly int _maxWordsLength;
+        private readonly double _minKeywordFrequency;
+        private readonly HashSet<string> _stopWords;
+
+        public Rake(CultureInfo stopWordCulture,
+                    int minCharLength = 1,
+                    int maxWordsLength = 5,
+                    double minKeywordFrequency = 1)
+        {
+            _minCharLength = minCharLength;
+            _maxWordsLength = maxWordsLength;
+            _minKeywordFrequency = minKeywordFrequency;
+            _stopWords = StopwordsLoader.GetStopWords(stopWordCulture).ToHashSet();
+        }
+
+        public Dictionary<string, float> Run(string text)
+        {
+            string[] sentenceList = RakeHelpers.SplitSentences(text.ToLowerInvariant());
+
+            var phraseList = GenerateCandidateKeywords(sentenceList, _minCharLength, _maxWordsLength);
+
+            var wordScores = RakeHelpers.CalculateWordScores(phraseList);
+
+            var keywordCandidates = RakeHelpers.GenerateCandidateKeywordScores(phraseList, wordScores, _minKeywordFrequency);
+
+            return keywordCandidates
+                .OrderByDescending(pair => pair.Value)
+                .ToDictionary(pair => pair.Key, pair => pair.Value);
+        }
+
+        private List<string> GenerateCandidateKeywords(string[] sentenceList,
+                                                       int minCharLength,
+                                                       int maxWordsLength)
+        {
+            var phraseList = new List<string>();
+
+            var sb = new StringBuilder();
+
+            foreach (string sentence in sentenceList)
+            {
+                string sLowerCase = sentence.Trim();
+
+                var wordSplitter = new StringSplitter(sLowerCase.AsSpan(), ' ');
+
+                while (wordSplitter.TryGetNext(out var wordSpan))
+                {
+                    string word = wordSpan.ToString();
+
+                    if (_stopWords.Contains(word))
+                    {
+                        string phrase = sb.ToString().Trim();
+
+                        if (!string.IsNullOrWhiteSpace(phrase)
+                            && RakeHelpers.IsAcceptable(phrase, minCharLength, maxWordsLength))
+                        {
+                            phraseList.Add(phrase);
+                        }
+
+                        sb.Clear();
+                    }
+                    else
+                    {
+                        sb.Append(word).Append(' ');
+                    }
+                }
+
+                string p2 = sb.ToString().Trim();
+
+                if (!string.IsNullOrWhiteSpace(p2)
+                    && RakeHelpers.IsAcceptable(p2, minCharLength, maxWordsLength))
+                {
+                    phraseList.Add(p2);
+                }
+
+                sb.Clear();
+            }
+
+            return phraseList;
+        }
+    }
+}
