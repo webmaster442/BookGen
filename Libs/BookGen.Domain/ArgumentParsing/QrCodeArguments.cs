245ed412789d5e05752a62146aee58063d4e33df
webmaster442 <ruzsinszki.gabor@gmail.com>
2022-11-01 21:18:46 +0100

    Preparations for QR-Code generator module

---
+++ Libs/BookGen.Domain/ArgumentParsing/QrCodeArguments.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2022 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using BookGen.Interfaces;
+
+namespace BookGen.Domain.ArgumentParsing
+{
+    public class QrCodeArguments : ArgumentsBase
+    {
+        [Switch("o", "output", true)]
+        public FsPath Output { get; set; }
+
+        [Switch("s", "size")]
+        public int Size { get; set; }
+
+        public QrCodeArguments()
+        {
+            Output = FsPath.Empty;
+            Size = 200;
+        }
+
+        public override bool Validate()
+        {
+            return
+                !FsPath.IsEmptyPath(Output)
+                && Size > 10
+                && Size < 1000
+                && (Output.Extension == ".png" || Output.Extension == ".svg");
+        }
+    }
+}
---
+++ Libs/BookGen.DomainServices/WebServices/GoQrMeParams.cs
@@ -0,0 +1,18 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2022 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+namespace BookGen.DomainServices.WebServices
+{
+    public static class GoQrMeParams
+    {
+        public const string ApiUrl = "https://api.qrserver.com/v1/create-qr-code/";
+        public const string DataParam = "data";
+        public const int MaxDataLength = 900;
+        public const string SizeParam = "size";
+        public const int MinimumSize = 10;
+        public const int MaximumSizer = 1000;
+        public const string FormatParam = "format";
+    }
+}
