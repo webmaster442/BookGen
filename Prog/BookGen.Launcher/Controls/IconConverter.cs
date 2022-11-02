d10c84812c8784cff52480325ca2ec360f6e5037
webmaster442 <ruzsinszki.gabor@gmail.com>
2022-11-02 21:27:45 +0100

    Added more icons to launcher

--- Prog/BookGen.Launcher/Controls/IconConverter.cs
+++ Prog/BookGen.Launcher/Controls/IconConverter.cs
@@ -19,6 +19,9 @@ namespace BookGen.Launcher.Controls
         {
             return tFrom.Extension switch
             {
+                ".c" or ".h" => GetResource("icon-c"),
+                ".cpp" or ".hpp" => GetResource("icon-cpp"),
+                ".cs" => GetResource("icon-csharp"),
                 ".css" => GetResource("icon-css"),
                 ".htm" or ".html" => GetResource("icon-html"),
                 ".js" => GetResource("icon-js"),
@@ -32,6 +35,8 @@ namespace BookGen.Launcher.Controls
                 ".doc" or ".docx" or ".xls" or ".xlsx" or ".ppt" or ".pptx" => GetResource("icon-office"),
                 ".xaml" => GetResource("icon-xaml"),
                 ".exe" => GetResource("icon-exe"),
+                ".py" => GetResource("icon-python"),
+                ".php" => GetResource("icon-php"),
                 ".zip" or ".7z" or ".rar" or ".gz" or ".tar" => GetResource("icon-pack"),
                 _ => GetGenericIcon(tFrom.Extension)
             };
