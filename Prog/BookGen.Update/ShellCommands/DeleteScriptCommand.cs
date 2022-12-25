413de5e250d0698fa76af9dc3a79f5c463477776
webmaster442 <ruzsinszki.gabor@gmail.com>
2022-12-25 18:53:59 +0100

    Shell commands

---
+++ Prog/BookGen.Update/ShellCommands/DeleteScriptCommand.cs
@@ -0,0 +1,15 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2022 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using BookGen.Update.Infrastructure;
+
+namespace BookGen.Update.ShellCommands;
+
+internal class DeleteScriptCommand : IShellCommand
+{
+    public string ToBash() => "rm `basename \"$0\"`";
+
+    public string ToPowerShell() => "Remove-Item -Force \"$PSCommandPath\"";
+}
---
+++ Prog/BookGen.Update/ShellCommands/MoveCommand.cs
@@ -0,0 +1,18 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2022 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using BookGen.Update.Infrastructure;
+
+namespace BookGen.Update.ShellCommands;
+
+internal sealed class MoveCommand : IShellCommand
+{
+    public required string Source { get; init; }
+    public required string Target { get; init; }
+
+    public string ToBash() => $"mv \"{Source}\" \"{Target}\"";
+
+    public string ToPowerShell() => $"Move-Item -Force \"{Source}\" \"{Target}\"";
+}
