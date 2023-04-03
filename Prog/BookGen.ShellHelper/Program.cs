616edaf2b8dad12379a54d2e54f46ccbd5c34a07
webmaster442 <ruzsinszki.gabor@gmail.com>
2023-04-03 17:23:13 +0200

    Shell Helper: Top level statements

--- Prog/BookGen.ShellHelper/Program.cs
+++ Prog/BookGen.ShellHelper/Program.cs
@@ -1,56 +1,47 @@
 ﻿//-----------------------------------------------------------------------------
-// (c) 2021 Ruzsinszki Gábor
+// (c) 2021-2023 Ruzsinszki Gábor
 // This code is licensed under MIT license (see LICENSE for details)
 //-----------------------------------------------------------------------------
 
-using BookGen.DomainServices;
 using System.Runtime.CompilerServices;
+
+using BookGen.DomainServices;
 
 [assembly: InternalsVisibleTo("BookGen.Tests")]
 
-namespace BookGen.ShellHelper
+const string promptMode = "prompt";
+const int timeOut = 10;
+
+if (args.Length == 2
+    && args[0] == promptMode)
 {
-    public static class Program
+    DoPrompt(args[1]);
+}
+
+static void DoPrompt(string workDir)
+{
+    if (!string.IsNullOrEmpty(workDir)
+        && TestIfGitDir(workDir))
     {
-        private const string PromptMode = "prompt";
-        private const int TimeOut = 10;
+        var arguments = new string[] { "status", "-b", "-s", "--porcelain=2" };
 
-        public static void Main(string[] arguments)
+        var (exitcode, output) = ProcessRunner.RunProcess("git", arguments, timeOut);
+        if (exitcode == 0)
         {
-            //Console.ReadKey();
-            if (arguments.Length == 2
-                && arguments[0] == PromptMode)
-            {
-                DoPrompt(arguments[1]);
-            }
-        }
-
-        private static void DoPrompt(string workDir)
-        {
-            if (!string.IsNullOrEmpty(workDir)
-                && TestIfGitDir(workDir))
-            {
-                var arguments = new string[] { "status", "-b", "-s", "--porcelain=2" };
-
-                var (exitcode, output) = ProcessRunner.RunProcess("git", arguments, TimeOut);
-                if (exitcode == 0)
-                {
-                    var status = GitParser.ParseStatus(output);
-                    Console.WriteLine("({0}) ↓: {1} ↑: {2} M: {3}",
-                                      status.BranchName,
-                                      status.IncommingCommits,
-                                      status.OutGoingCommits,
-                                      status.NotCommitedChanges);
-                }
-            }
-        }
-
-        private static bool TestIfGitDir(string workDir)
-        {
-            var arguments = new string[] { "status", "2"};
-
-            var (exitcode, _) = ProcessRunner.RunProcess("git", arguments, TimeOut, workDir);
-            return exitcode == 0;
+            var status = GitParser.ParseStatus(output);
+            Console.WriteLine("({0}) ↓: {1} ↑: {2} M: {3}",
+                              status.BranchName,
+                              status.IncommingCommits,
+                              status.OutGoingCommits,
+                              status.NotCommitedChanges);
         }
     }
+}
+
+static bool TestIfGitDir(string workDir)
+{
+    var arguments = new string[] { "status", "2" };
+
+    var (exitcode, _) = ProcessRunner.RunProcess("git", arguments, timeOut, workDir);
+    return exitcode == 0;
 }
