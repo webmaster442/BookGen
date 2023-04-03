ed932ab883c1f619c0205716bcce06b7d14b4c3a
webmaster442 <ruzsinszki.gabor@gmail.com>
2023-04-03 17:35:30 +0200

    Launcher: re-added function to install terminal profile

--- Prog/BookGen.Launcher/MainWindowViewModel.cs
+++ Prog/BookGen.Launcher/MainWindowViewModel.cs
@@ -1,7 +1,9 @@
 ﻿//-----------------------------------------------------------------------------
-// (c) 2021-2022 Ruzsinszki Gábor
+// (c) 2021-2023 Ruzsinszki Gábor
 // This code is licensed under MIT license (see LICENSE for details)
 //-----------------------------------------------------------------------------
+
+using BookGen.DomainServices;
 
 namespace BookGen.Launcher
 {
@@ -16,6 +18,7 @@ namespace BookGen.Launcher
         public RelayCommand ClosePopupCommand { get; }
         public RelayCommand OpenSettingsCommand { get; }
         public RelayCommand StartCommand { get; }
+        public RelayCommand TerminalInstallCommand { get; }
         public RelayCommand<string> OpenBrowserCommand { get; }
 
         public string AppTitle
@@ -57,11 +60,23 @@ namespace BookGen.Launcher
         public MainWindowViewModel()
         {
             _popupTitle = string.Empty;
+            TerminalInstallCommand = new RelayCommand(OnTerminalInstall);
             ClosePopupCommand = new RelayCommand(OnClosePopup);
             OpenBrowserCommand = new RelayCommand<string>(OnOpenBrowser);
             OpenSettingsCommand = new RelayCommand(OnOpenSettings);
             StartCommand = new RelayCommand(OnStart);
             OnStart();
+        }
+
+        private void OnTerminalInstall()
+        {
+            var result =TerminalProfileInstaller.TryInstall();
+            if (result == null)
+                MessageBox.Show("Windows Terminal not installed. Profile installation can't continue", "Terminal install", MessageBoxButton.OK, MessageBoxImage.Warning);
+            else if (result == true)
+                MessageBox.Show("Terminal profile installed", "Terminal install", MessageBoxButton.OK, MessageBoxImage.Information);
+            else
+                MessageBox.Show("Terminal profile install failed", "Terminal install", MessageBoxButton.OK, MessageBoxImage.Error);
         }
 
         private void OnStart()
