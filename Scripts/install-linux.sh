d48c4c613c0be7df3e27836de6c7bf5e55feecdb
webmaster442 <ruzsinszki.gabor@gmail.com>
2023-05-16 21:28:07 +0200

    Linux installer script

--- Scripts/install-linux.sh
+++ Scripts/install-linux.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
+URL_FILE="https://github.com/webmaster442/BookGen/blob/master/.github/linux-latest.txt"
 TARGET_DIR="/opt/bookgen"
+DOWNLOAD_URL=$(curl -s "$URL_FILE")
+curl -o bookgen.tar.gz "$DOWNLOAD_URL"
+tar -xf bookgen.tar.gz -C "$TARGET_DIR"
+rm bookgen.tar.gz
+
+SCRIPT_PATH="/usr/bin/bookgen"
+COMMAND="/opt/bookgen/BookGen"
+cat <<EOF > "$SCRIPT_PATH"
+#!/bin/bash
+$COMMAND "\$@"
+EOF
+
+chmod +x "$SCRIPT_PATH"
