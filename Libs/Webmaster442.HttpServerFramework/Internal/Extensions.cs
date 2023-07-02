ba3a9fa846aeb51c9bd20b9b4623dab9d4f9a61f
webmaster442 <ruzsinszki.gabor@gmail.com>
2023-07-02 17:52:56 +0200

    Compression based on encoding type feature abandoned

--- Libs/Webmaster442.HttpServerFramework/Internal/Extensions.cs
+++ Libs/Webmaster442.HttpServerFramework/Internal/Extensions.cs
@@ -5,8 +5,6 @@
 
 using System.Globalization;
 
-using Webmaster442.HttpServerFramework.Domain;
-
 namespace Webmaster442.HttpServerFramework.Internal;
 
 internal static class Extensions
@@ -19,57 +17,5 @@ internal static class Extensions
             dateTime = dateTime.ToUniversalTime();
         }
         return $"{dateTime.ToString("ddd, dd MMM yyyy HH:mm:ss", culture)} GMT";
-    }
-
-    public static bool GetAcceptedEncodings(this HttpRequest request, out EncodingType encodingType)
-    {
-        if (!request.Headers.ContainsKey("Accept-Encoding"))
-        {
-            encodingType = EncodingType.None;
-            return false;
-        }
-
-        EncodingType results = EncodingType.None;
-
-        var values = request.Headers["Accept-Encoding"]
-            .Split(',', StringSplitOptions.TrimEntries);
-
-        foreach (string value in values)
-        {
-            if (!TryParseKnownEncoding(value, out encodingType))
-            {
-                encodingType = EncodingType.None;
-                return false;
-            }
-
-            results |= encodingType;
-        }
-
-        encodingType = results;
-        return true;
-    }
-
-    private static bool TryParseKnownEncoding(string value, out EncodingType encodingType)
-    {
-        switch (value)
-        {
-            case "gzip":
-                encodingType = EncodingType.Gzip;
-                return true;
-            case "deflate":
-                encodingType = EncodingType.Deflate;
-                return true;
-            case "br":
-                encodingType = EncodingType.Brotli;
-                return true;
-            case "compress":
-            case "identity":
-            case "*":
-                encodingType = EncodingType.None;
-                return true;
-            default:
-                encodingType = EncodingType.None;
-                return false;
-        }
     }
 }
