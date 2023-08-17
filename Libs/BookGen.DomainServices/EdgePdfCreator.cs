be76e2aa53760164bfc8d11fdb55cffd1e14887d
webmaster442 <ruzsinszki.gabor@gmail.com>
2023-08-17 21:16:01 +0200

    PDF Creator

---
+++ Libs/BookGen.DomainServices/EdgePdfCreator.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+using BookGen.Interfaces;
+
+namespace BookGen.DomainServices;
+
+public static class EdgePdfCreator
+{
+    public static async Task CreatePdf(FsPath htmlFile, FsPath targetFile)
+    {
+        string[] arguments = new[]
+        {
+            "--headless",
+            "--disable-gpu",
+            $"--print-to-pdf=\"{targetFile}\"",
+            "--disable-extensions",
+            "--no-pdf-header-footer",
+            "--disable-popup-blocking",
+            "--run-all-compositor-stages-before-draw",
+            "--disable-checker-imaging",
+            $"file:///{htmlFile}"
+        };
+
+        using (var process = new Process())
+        {
+            process.StartInfo.FileName = "msedge.exe";
+            process.StartInfo.Arguments = string.Join(' ', arguments);
+            process.StartInfo.UseShellExecute = false;
+            process.Start();
+            await process.WaitForExitAsync();
+        }
+    }
+}
