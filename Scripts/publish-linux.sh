c3cc58265b5534c88a784bbbb11ebca82aaf4479
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-02-04 20:44:12 +0100

    Linux packaging

--- Scripts/publish-linux.sh
+++ Scripts/publish-linux.sh
@@ -2,14 +2,20 @@
 cd ..
 cp BookGen.sln BookGen-Linux.sln
 dotnet sln BookGen-Linux.sln remove Prog/BookGen.Launcher
-dotnet sln BookGen-Linux.sln remove Prog/BookGen.Update
 dotnet publish BookGen-Linux.sln -c Release -r linux-x64 -p:PublishReadyToRun=true --self-contained true -o bin/publish-linux
+
 cd bin/publish-linux
 ./BookGen Md2HTML -i ../../Commands.md -ns -o Commands.html
 ./BookGen Md2HTML -i ../../Changelog.md -ns -o ChangeLog.html
 ./BookGen Md2HTML -i ../../notes.md -ns -o RelaseNotes.html
-cd bin/publish-linux
 rm tidy.exe
-tar -zcvf ../publish-linux.tar.gz .
+
 cd ..
-sha256sum publish-linux.tar.gz > linux-hash.txt
+mkdir -p package/usr/local/bin
+mkdir -p package/DEBIAN
+cd ..
+cp Setup/linux-start.sh bin/package/usr/local/bin/bookgen
+cp Setup/package.control bin/package/DEBIAN/control
+cd bin
+chmod 775 ./package/DEBIAN/
+dpkg-deb --build ./package/
