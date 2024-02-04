8ad726c296cd9bc539504dd4811c7db3521a8b18
webmaster442 <ruzsinszki.gabor@gmail.com>
2024-02-04 21:35:59 +0100

    Linux package creation

--- Scripts/publish-linux.sh
+++ Scripts/publish-linux.sh
@@ -12,10 +12,18 @@ rm tidy.exe
 
 cd ..
 mkdir -p package/usr/local/bin
+mkdir -p package//opt/bookgen
 mkdir -p package/DEBIAN
 cd ..
+
 cp Setup/linux-start.sh bin/package/usr/local/bin/bookgen
 cp Setup/package.control bin/package/DEBIAN/control
+cp -r bin/publish-linux/* bin/package/opt/bookgen
+
 cd bin
 chmod 775 ./package/DEBIAN/
+chmod +x ./package/usr/local/bin/bookgen
+chmod +x ./package/opt/bookgen/BookGen
 dpkg-deb --build ./package/
+mv package.deb bookgen.deb 
+rm -r package
