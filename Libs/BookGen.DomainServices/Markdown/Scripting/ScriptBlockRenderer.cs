7548c62fe0f1519d0e1b8e16bdda0b98d5587a1a
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-06-16 17:39:06 +0200

    WIP: Scripting support

---
+++ Libs/BookGen.DomainServices/Markdown/Scripting/ScriptBlockRenderer.cs
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2024 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using Markdig.Renderers;
+using Markdig.Renderers.Html;
+
+namespace BookGen.DomainServices.Markdown.Scripting;
+
+internal sealed class ScriptBlockRenderer : HtmlObjectRenderer<ScriptBlock>
+{
+    private readonly CsharpScriptExecutor _scriptExecutor;
+
+    public ScriptBlockRenderer(CsharpScriptExecutor scriptExecutor)
+    {
+        _scriptExecutor = scriptExecutor;
+    }
+
+    protected override void Write(HtmlRenderer renderer, ScriptBlock obj)
+    {
+        var result = _scriptExecutor.Execute(obj.GetScript()).Result;
+        renderer.Write(result);
+    }
+}
