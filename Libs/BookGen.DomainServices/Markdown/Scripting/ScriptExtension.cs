9346eb4887aae6eb49bbf468fdc38eb2a6fdeb74
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-06-17 19:20:10 +0200

    Scripting extension

---
+++ Libs/BookGen.DomainServices/Markdown/Scripting/ScriptExtension.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2024 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using Markdig;
+using Markdig.Renderers;
+
+namespace BookGen.DomainServices.Markdown.Scripting;
+
+internal sealed class ScriptExtension : IMarkdownExtension
+{
+    private readonly CsharpScriptExecutor _scriptExecutor;
+
+    public ScriptExtension()
+    {
+        _scriptExecutor = new CsharpScriptExecutor();
+    }
+
+    public void Setup(MarkdownPipelineBuilder pipeline)
+    {
+        pipeline.BlockParsers.AddIfNotAlready(new ScriptBlockParser());
+    }
+
+    public void Setup(MarkdownPipeline pipeline, IMarkdownRenderer renderer)
+    {
+        if (renderer is HtmlRenderer htmlRenderer)
+        {
+            htmlRenderer.ObjectRenderers.AddIfNotAlready(new ScriptBlockRenderer(_scriptExecutor));
+        }
+    }
+}
---
+++ Libs/BookGen.DomainServices/Markdown/Scripting/ScriptExtensions.cs
@@ -0,0 +1,18 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2024 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using Markdig;
+using Markdig.Renderers;
+
+namespace BookGen.DomainServices.Markdown.Scripting;
+
+internal static class ScriptExtensions
+{
+    public static MarkdownPipelineBuilder UseScripting(this MarkdownPipelineBuilder pipelineBuilder)
+    {
+        pipelineBuilder.Extensions.AddIfNotAlready(new ScriptExtension());
+        return pipelineBuilder;
+    }
+}
