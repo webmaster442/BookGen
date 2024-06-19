bf11a4595427b5c22d236b9634f8d9f4dda38c3c
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-06-19 20:45:22 +0200

    Issue fix in git autocomplete

--- Libs/BookGen.DomainServices/ShellAutoCompleteFilter.cs
+++ Libs/BookGen.DomainServices/ShellAutoCompleteFilter.cs
@@ -11,19 +11,39 @@ public static class ShellAutoCompleteFilter
     {
         if (candidates.Count < 1
             || string.IsNullOrEmpty(input)
-            || cursorposition < 0 
+            || cursorposition < 0
             || cursorposition > input.Length)
         {
-            return Array.Empty<string>();
+            yield break;
         }
 
         string prefix = input[..cursorposition];
-        int prefixLength = cursorposition >= prefix.Length ? prefix.Length - 1: cursorposition;
+        int prefixLength = cursorposition >= prefix.Length ? prefix.Length - 1 : cursorposition;
 
         var filteredCommands = candidates
-            .Where(cmd => cmd.StartsWith(prefix))
-            .Select(cmd => cmd.Substring(prefixLength).TrimStart());
+            .Where(cmd => cmd.StartsWith(prefix));
 
-        return filteredCommands;
+        foreach (var filtered in filteredCommands)
+        {
+            var (start, _) = GetWordPositions(filtered).FirstOrDefault(p => cursorposition >= p.start && cursorposition <= p.end);
+            yield return filtered[start..];
+        }
+    }
+
+    internal static IEnumerable<(int start, int end)> GetWordPositions(string str)
+    {
+        int start = 0;
+        int pos = 0;
+        foreach (var c in str)
+        {
+            if (char.IsWhiteSpace(c))
+            {
+                var item = (start, pos);
+                start = pos + 1;
+                yield return item;
+            }
+            ++pos;
+        }
+        yield return (start, pos);
     }
 }
