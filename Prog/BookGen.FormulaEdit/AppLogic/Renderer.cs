b14de7c0637dfb32567ed7245fec093402acba0e
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-06-26 16:35:08 +0200

    Branding: Remove old

--- Prog/BookGen.FormulaEdit/AppLogic/Renderer.cs
+++ Prog/BookGen.FormulaEdit/AppLogic/Renderer.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 
 using WpfMath.Converters;
