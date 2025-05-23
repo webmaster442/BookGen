2a49381abbda8c4cef9a074d86b786fe4abc4291
webmaster442 <ruzsinszki.gabor@gmail.com>
2025-05-23 17:39:22 +0200

    Bootstappers

--- scripts/build-windows.ps1
+++ scripts/build-windows.ps1
@@ -1,3 +1,6 @@
+$psCoreUrl = "https://github.com/PowerShell/PowerShell/releases/download/v7.4.10/PowerShell-7.4.10-win-x64.zip"
+$nodeUrl = "https://nodejs.org/dist/v22.16.0/node-v22.16.0-win-x64.zip"
+
 if ($Host.version.Major -lt 7)
 {
 	Write-Host "This script requires powershell 7 or newer"
@@ -7,6 +10,8 @@ if ($Host.version.Major -lt 7)
 
 $ErrorActionPreference = "Stop"
 
+clear
+
 cd ..
 
 Write-Host "Updating Getting started doc"
@@ -15,5 +20,27 @@ Show-Markdown getting-started.md > Source\BookGen.Contents\getting-started.mdr
 Write-Host "Publish..."
 dotnet publish BookGen.slnx -c Release -r win-x64 -p:PublishReadyToRun=true --self-contained true -o bin\publish\app
 
+Write-Host "Generating documents"
 cd bin\publish\app
 .\BookGen.exe Schemas
+.\BookGen.exe md2html -i Schemas.md -o ..\Schemas.html -t "BookGen Schemas"
+.\BookGen.exe Md2HTML -i ..\..\..\Changelog.md -o ..\ChangeLog.html -t "Change Log"
+.\BookGen.exe Md2HTML -i ..\..\..\Commands.md -o ..\Commands.html -t "BookGen Commands"
+del schemas.md
+
+cd ..\..\..\Bootstrappers
+dotnet build -c Release
+cd ..
+copy-item bin\bootstaper\Release\BookGen.exe bin\Publish
+
+Write-Host "Getting powershell core..."
+Invoke-WebRequest -Uri $psCoreUrl -OutFile pwsh.zip
+Expand-Archive -Path pwsh.zip -DestinationPath "powershell"
+Remove-Item pwsh.zip
+
+Write-Host "Getting Node.js..."
+Invoke-WebRequest -Uri $nodeUrl -OutFile node.zip
+Expand-Archive -Path node.zip -DestinationPath data
+Remove-Item node.zip
+
+cd ..\..\..\scripts
