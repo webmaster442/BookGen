35f8994a29ec4698a082af32448894f8560bed97
webmaster442 <ruzsinszki.gabor@gmail.com>
2025-05-23 17:13:44 +0200

    Schemas command fixes

--- Source/BookGen/Commands/SchemasCommand.cs
+++ Source/BookGen/Commands/SchemasCommand.cs
@@ -1,6 +1,11 @@
-﻿using System.Text.Json;
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Text.Json.Schema;
 using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
 
 using Bookgen.Lib;
 using Bookgen.Lib.Domain.IO;
@@ -10,24 +15,65 @@ using BookGen.Cli;
 using BookGen.Cli.Annotations;
 using BookGen.Vfs;
 
+using Microsoft.Extensions.Logging;
+
 namespace BookGen.Commands;
 
 [CommandName("schemas")]
 internal class SchemasCommand : Command<BookGenArgumentBase>
 {
     private readonly IWritableFileSystem _writableFileSystem;
+    private readonly ILogger _logger;
     private readonly JsonSerializerOptions _options;
+    private readonly JsonSchemaExporterOptions _exporterOptions;
 
-    public SchemasCommand(IWritableFileSystem writableFileSystem)
+    public SchemasCommand(IWritableFileSystem writableFileSystem, ILogger logger)
     {
         _writableFileSystem = writableFileSystem;
-        _options = new JsonSerializerOptions
+        _logger = logger;
+        _options = new JsonSerializerOptions(JsonSerializerOptions.Default)
         {
             WriteIndented = true,
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
             Converters =
             {
                 new JsonStringEnumConverter()
+            },
+        };
+        _exporterOptions = new()
+        {
+            TransformSchemaNode = (context, schema) =>
+            {
+                // Determine if a type or property and extract the relevant attribute provider.
+                ICustomAttributeProvider? attributeProvider = context.PropertyInfo is not null
+                    ? context.PropertyInfo.AttributeProvider
+                    : context.TypeInfo.Type;
+
+                // Look up any description attributes.
+                DescriptionAttribute? descriptionAttr = attributeProvider?
+                    .GetCustomAttributes(inherit: true)
+                    .Select(attr => attr as DescriptionAttribute)
+                    .FirstOrDefault(attr => attr is not null);
+
+                // Apply description attribute to the generated schema.
+                if (descriptionAttr != null)
+                {
+                    if (schema is not JsonObject jObj)
+                    {
+                        // Handle the case where the schema is a Boolean.
+                        JsonValueKind valueKind = schema.GetValueKind();
+                        Debug.Assert(valueKind is JsonValueKind.True or JsonValueKind.False);
+                        schema = jObj = new JsonObject();
+                        if (valueKind is JsonValueKind.False)
+                        {
+                            jObj.Add("not", true);
+                        }
+                    }
+
+                    jObj.Insert(0, "description", descriptionAttr.Description);
+                }
+
+                return schema;
             }
         };
     }
@@ -39,13 +85,14 @@ internal class SchemasCommand : Command<BookGenArgumentBase>
         markdownBuilder.Heading(1, "Bookgen Schemas")
             .Paragraph("This document contains the schemas used by Bookgen.")
             .Heading(2, "Bookgen.json")
-            .CodeBlock(_options.GetJsonSchemaAsNode(typeof(Config)).ToJsonString(), "json")
+            .CodeBlock(_options.GetJsonSchemaAsNode(typeof(Config), _exporterOptions).ToString(), "json")
             .Heading(2, "Table of contents file")
-            .CodeBlock(_options.GetJsonSchemaAsNode(typeof(TableOfContents)).ToJsonString(), "json")
+            .CodeBlock(_options.GetJsonSchemaAsNode(typeof(TableOfContents), _exporterOptions).ToString(), "json")
             .Heading(3, "Page frontmatter")
             .Paragraph("Each page in the table of contents must have a YAML front matter.")
-            .CodeBlock(_options.GetJsonSchemaAsNode(typeof(FrontMatter)).ToJsonString(), "json");
+            .CodeBlock(_options.GetJsonSchemaAsNode(typeof(FrontMatter), _exporterOptions).ToString(), "json");
 
+        _logger.LogInformation("Writing schemas.md...");
         _writableFileSystem.Scope = arguments.Directory;
         _writableFileSystem.WriteAllText("schemas.md", markdownBuilder.ToString());
 
--- Source/Bookgen.Lib/Domain/IO/Configuration/CssClasses.cs
+++ Source/Bookgen.Lib/Domain/IO/Configuration/CssClasses.cs
@@ -1,23 +1,42 @@
-﻿namespace Bookgen.Lib.Domain.IO.Configuration;
+﻿using System.ComponentModel;
+
+namespace Bookgen.Lib.Domain.IO.Configuration;
 
 public sealed class CssClasses
 {
+    [Description("css classes aplied to <h1> element")]
     public string H1 { get; init; } = string.Empty;
+
+    [Description("css classes aplied to <h2> element")]
     public string H2 { get; init; } = string.Empty;
+
+    [Description("css classes aplied to <h3> element")]
     public string H3 { get; init; } = string.Empty;
+
+    [Description("css classes aplied to <img> element")]
     public string Img { get; init; } = string.Empty;
 
+    [Description("css classes aplied to <table> element")]
     public string Table { get; init; } = string.Empty;
+
+    [Description("css classes aplied to <bockquote> element")]
     public string Blockquote { get; init; } = string.Empty;
 
+    [Description("css classes aplied to <figure> element")]
     public string Figure { get; init; } = string.Empty;
 
+    [Description("css classes aplied to <figcaption> element")]
     public string FigureCaption { get; init; } = string.Empty;
 
+    [Description("css classes aplied to <a> element")]
     public string Link { get; init; } = string.Empty;
 
+    [Description("css classes aplied to <ol> element")]
     public string Ol { get; init; } = string.Empty;
+
+    [Description("css classes aplied to <ul> element")]
     public string Ul { get; init; } = string.Empty;
 
+    [Description("css classes aplied to <li> element")]
     public string Li { get; init; } = string.Empty;
 }
