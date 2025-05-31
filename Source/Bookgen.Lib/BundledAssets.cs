7d1087e0f500d6dce9dcb4bb4437e1c5b076c6b5
webmaster442 <ruzsinszki.gabor@gmail.com>
2025-05-31 11:14:14 +0200

    Templates update

--- Source/Bookgen.Lib/BundledAssets.cs
+++ Source/Bookgen.Lib/BundledAssets.cs
@@ -6,6 +6,6 @@ public static class BundledAssets
     public const string BookGenCss = "bookgen.min.css";
     public const string TemplateSinglePage = "Md2Html.html";
     public const string ProtectHtml = "protect.html";
-    public const string TemplateStaticWeb = "Md2Html.html";
+    public const string TemplateStaticWeb = "Static.html";
     public const string TemplatePrint = "Print.html";
 }
