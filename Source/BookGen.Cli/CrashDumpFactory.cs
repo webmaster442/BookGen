47d3798ec3f008632b52968d2b2192cf341b8030
webmaster442 <ruzsinszki.gabor@gmail.com>
2025-06-01 16:33:57 +0200

    CrashDump improvements

--- Source/BookGen.Cli/CrashDumpFactory.cs
+++ Source/BookGen.Cli/CrashDumpFactory.cs
@@ -6,16 +6,28 @@ public static class CrashDumpFactory
 {
     public static void TryCreateCrashDump(Exception ex)
     {
-        var fileName = $"crash_{DateTime.Now:yyyy-MM-dd-HH-mm-ss}.txt";
-        var dir = Path.Combine(AppContext.BaseDirectory, fileName);
+        var fileName = $"bookgen_crash_{DateTime.Now:yyyy-MM-dd-HH-mm-ss}.txt";
+        string dir = Path.Combine(AppContext.BaseDirectory, fileName);
         StringBuilder content = new();
 
         content
-            .AppendLine($"Crash reason: {ex.Message}")
-            .AppendLine("Stack trace:")
-            .AppendLine(ex.StackTrace)
+            .AppendLine(new string('-', 80))
             .AppendLine("Environment:")
-            .AppendLine($"Command line: {string.Join(' ', Environment.GetCommandLineArgs())}");
+            .AppendLine($"Command line: {string.Join(' ', Environment.GetCommandLineArgs())}")
+            .AppendLine($"Crash reason: {ex.Message}")
+            .AppendLine(new string('-', 80))
+            .AppendLine("Stack trace:")
+            .AppendLine(ex.StackTrace);
+
+        if (ex.InnerException != null)
+        {
+            content
+                .AppendLine(new string('-', 80))
+                .AppendLine($"Inner exception: {ex.InnerException.Message}")
+                .AppendLine(new string('-', 80))
+                .AppendLine(ex.InnerException.StackTrace);
+        }
+
 
 #pragma warning disable RCS1075 // Avoid empty catch clause that catches System.Exception
         try
