4657d3d7d6d9d9bc09bdf9670199e1abb307acda
webmaster442 <ruzsinszki.gabor@gmail.com>
2026-02-21 11:53:45 +0100

    Added new tests

--- Test/Bookgen.Tests/Lib/UT_MarkdownConverter.cs
+++ Test/Bookgen.Tests/Lib/UT_MarkdownConverter.cs
@@ -13,9 +13,9 @@ namespace Bookgen.Tests.Lib;
 
 internal class UT_MarkdownConverter
 {
-    private Mock<IImgService>? _imgServiceMock;
-    private string? _markdown;
-    private string? _soruceCode;
+    private Mock<IImgService> _imgServiceMock;
+    private string _markdown;
+    private string _soruceCode;
     private readonly IEqualityComparer<string?> comparer = new LineEndingIgnoreComparer();
 
     [SetUp]
@@ -534,6 +534,66 @@ internal class UT_MarkdownConverter
             <h1 id="t4">t4</h1>
             
             """;
+
+        string result = sut.RenderMarkdownToHtml(input);
+
+        Assert.That(result, Is.EqualTo(expected).Using(comparer));
+    }
+
+    [Test]
+    public void EnsureThat_TerminalRenderingWorks()
+    {
+        using var settings = new RenderSettings(_imgServiceMock.Object)
+        {
+            CssClasses = new CssClasses(),
+            DeleteFirstH1 = false,
+            HostUrl = null,
+            PrismJsInterop = null,
+            AutoEmbedSupportedLinks = true,
+        };
+
+        using var sut = new MarkdownConverter(settings);
+
+        string input = """
+            ```terminal
+            this is terminal output
+            ```
+            """;
+
+        string expected = """
+            <div style="margin-bottom: 1rem;" class="terminaloutput">
+            <div style="background-color: #877EC2; color: #eee8d6; padding: 3px;">$</div>
+            <pre style="text-align: left; font-size: 1.1em; line-height: 1.5; margin: 0px; padding: 8px; background-color: #282c34; color: #DCDFE4; font-family: Monaco, Menlo, Consolas, 'Courier New', monospace; word-break: break-all; word-wrap: break-word; overflow: auto;"><code style="tab-size: 4;">this is terminal output</code></pre>
+            </div>
+            """;
+
+        string result = sut.RenderMarkdownToHtml(input);
+
+        Assert.That(result, Is.EqualTo(expected).Using(comparer));
+    }
+
+    [Test]
+    public void EnsrureThat_Youtube_Autolink_Works()
+    {
+        string input = """
+            https://www.youtube.com/watch?v=D5ivt3hNAW8
+            """;
+
+        string expected = """
+            <p><iframe width="560" height="315" src="https://www.youtube.com/embed/D5ivt3hNAW8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></p>
+            
+            """;
+
+        using var settings = new RenderSettings(_imgServiceMock.Object)
+        {
+            CssClasses = new CssClasses(),
+            DeleteFirstH1 = false,
+            HostUrl = null,
+            PrismJsInterop = null,
+            AutoEmbedSupportedLinks = true,
+        };
+
+        using var sut = new MarkdownConverter(settings);
 
         string result = sut.RenderMarkdownToHtml(input);
 
