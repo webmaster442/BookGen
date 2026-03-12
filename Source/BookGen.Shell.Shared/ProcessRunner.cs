da6f4d297feca38979cfff4d8d2f9c03827f6471
webmaster442 <ruzsinszki.gabor@gmail.com>
2026-03-12 16:48:15 +0100

    Removed a bunch of dead code in process runner

--- Source/BookGen.Shell.Shared/ProcessRunner.cs
+++ Source/BookGen.Shell.Shared/ProcessRunner.cs
@@ -1,14 +1,9 @@
 ﻿//-----------------------------------------------------------------------------
-// (c) 2019-2025 Ruzsinszki Gábor
+// (c) 2019-2026 Ruzsinszki Gábor
 // This code is licensed under MIT license (see LICENSE for details)
 //-----------------------------------------------------------------------------
 
 using System.Diagnostics;
-using System.Text;
-
-using Microsoft.Extensions.Logging;
-
-using Webmaster442.WindowsTerminal;
 
 namespace BookGen.Shell.Shared;
 
@@ -16,9 +11,9 @@ public static class ProcessRunner
 {
     public static void OpenUrl(string url)
     {
-        if (!url.StartsWith("http://") && !url.StartsWith("https://"))
+        if (!Uri.TryCreate(url, UriKind.Absolute, out _))
             throw new ArgumentException("invalid url", nameof(url));
-
+           
         using (var process = new Process())
         {
             process.StartInfo.UseShellExecute = true;
@@ -28,53 +23,10 @@ public static class ProcessRunner
     }
 
     public static (int exitcode, string output, string error) RunProcess(string programPath,
-                                                          string argument,
-                                                          int timeOutSeconds,
-                                                          string? workdir = null)
-    {
-        return RunProcess(programPath, new string[] { argument }, timeOutSeconds, workdir);
-    }
-
-    public static void RunProcess(string program, string arguments)
-    {
-        using (var process = new Process())
-        {
-            process.StartInfo.FileName = program;
-            process.StartInfo.Arguments = arguments;
-            process.StartInfo.UseShellExecute = false;
-            process.Start();
-        }
-    }
-
-    private static void RunShell(string shell, string arguments, ILogger log)
-    {
-        try
-        {
-            using var process = new Process();
-            process.StartInfo.FileName = shell;
-            process.StartInfo.Arguments = arguments;
-            process.StartInfo.UseShellExecute = false;
-            process.Start();
-            process.WaitForExit();
-        }
-        catch (Exception e)
-        {
-            log.LogWarning(e, e.Message);
-        }
-    }
-
-    public static void RunCmdScript(string shellScript, ILogger log)
-        => RunShell("cmd.exe", $"\"{shellScript}\"", log);
-
-    public static void RunPowershellScript(string shellScript, ILogger log)
-    {
-        InstallResult installResult = InstallDetector.GetInstallResult();
-        if (installResult.IsPsCoreInstalled)
-            RunShell(InstallDetector.PowershellCoreExe, $"-ExecutionPolicy Bypass -File \"{shellScript}\"", log);
-        else
-            RunShell("powershell.exe", $"-ExecutionPolicy Bypass -File \"{shellScript}\"", log);
-    }
-
+                                                                         string argument,
+                                                                         int timeOutSeconds,
+                                                                         string? workdir = null)
+        => RunProcess(programPath, [argument], timeOutSeconds, workdir);
 
     public static (int exitcode, string output, string error) RunProcess(string programPath,
                                                                          string[] arguments,
@@ -106,78 +58,6 @@ public static class ProcessRunner
             {
                 return (process.ExitCode, read.Result, errorRead.Result);
             }
-        }
-    }
-
-    private static IProgress<string>? _reporter;
-
-    public static int RunProcess(string programPath,
-                                 string[] arguments,
-                                 string? workdir,
-                                 IProgress<string> progress)
-    {
-        using (var process = new Process())
-        {
-            process.StartInfo.FileName = programPath;
-            SetArguments(process.StartInfo, arguments);
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.RedirectStandardOutput = true;
-            process.StartInfo.RedirectStandardError = true;
-            process.StartInfo.CreateNoWindow = true;
-            process.StartInfo.StandardOutputEncoding = Encoding.UTF8;
-            process.StartInfo.StandardErrorEncoding = Encoding.UTF8;
-            process.OutputDataReceived += OnOutput;
-            process.ErrorDataReceived += OnOutput;
-            _reporter = progress;
-
-            if (workdir != null)
-                process.StartInfo.WorkingDirectory = workdir;
-
-            process.Start();
-            process.BeginErrorReadLine();
-            process.BeginOutputReadLine();
-            process.WaitForExit();
-
-            process.OutputDataReceived -= OnOutput;
-            process.ErrorDataReceived -= OnOutput;
-            _reporter = null;
-
-            return process.ExitCode;
-        }
-    }
-
-    private static void OnOutput(object sender, DataReceivedEventArgs e)
-    {
-        if (e.Data != null)
-            _reporter?.Report(e.Data);
-    }
-
-    public static (string stdOut, string stdErr) GetCommandOutput(string program, string[] arguments, string stdIn, int timeoutSeconds)
-    {
-        using (var process = new Process())
-        {
-            process.StartInfo.FileName = program;
-            SetArguments(process.StartInfo, arguments);
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.RedirectStandardOutput = true;
-            process.StartInfo.RedirectStandardError = true;
-            process.StartInfo.RedirectStandardInput = true;
-            process.StartInfo.CreateNoWindow = true;
-            process.StartInfo.StandardInputEncoding = Encoding.UTF8;
-            process.StartInfo.StandardOutputEncoding = Encoding.UTF8;
-            process.StartInfo.StandardErrorEncoding = Encoding.UTF8;
-
-            Task timeout = Task.Delay(timeoutSeconds * 1000);
-            process.Start();
-            process.StandardInput.Write(stdIn);
-            process.StandardInput.Close();
-            Task<string> @out = process.StandardOutput.ReadToEndAsync();
-            Task<string> err = process.StandardError.ReadToEndAsync();
-            if (Task.WaitAny(timeout, @out, err) == 0)
-            {
-                return ("", "timeout");
-            }
-            return (@out.Result, err.Result);
         }
     }
 
