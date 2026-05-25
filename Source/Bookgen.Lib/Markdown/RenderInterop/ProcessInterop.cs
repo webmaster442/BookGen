05014ebfa467a9f131abbe8857450bb2b1a124c8
webmaster442 <ruzsinszki.gabor@gmail.com>
2026-05-25 13:12:49 +0200

    Work in progress process interop

---
+++ Source/Bookgen.Lib/Markdown/RenderInterop/ProcessInterop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Bookgen.Lib.Markdown.RenderInterop;
+
+internal static class ProcessInterop
+{
+    private static string GetBinary(string name)
+    {
+        return OperatingSystem.IsWindows()
+            ? Path.Combine(AppContext.BaseDirectory, $"{name}.exe")
+            : Path.Combine(AppContext.BaseDirectory, name);
+    }
+
+    public static string RunRatex(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
