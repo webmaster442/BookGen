800687332fa4d49945ae4df2a4dadc2110872851
webmaster442 <ruzsinszki.gabor@gmail.com>
2026-05-31 20:03:46 +0200

    Pipeline upgrade

--- download-assets-pipeline.sh
+++ download-assets-pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-mkdir -p "./Assets\tools"
+mkdir -p "./Assets/tools"
 cd "./Assets/tools"
-curl.exe -L -O "https://github.com/erweixin/RaTeX/releases/download/v0.1.10/ratex-cli-v0.1.10-aarch64-unknown-linux-musl.tar.gz"
-tar -xzf ./ratex-cli-v0.1.10-aarch64-unknown-linux-musl.tar.gz -C
+curl -L -O "https://github.com/erweixin/RaTeX/releases/download/v0.1.10/ratex-cli-v0.1.10-aarch64-unknown-linux-musl.tar.gz"
+tar -xzf ./ratex-cli-v0.1.10-aarch64-unknown-linux-musl.tar.gz -C .
 mv ./ratex-cli-v0.1.10-aarch64-unknown-linux-musl/render-svg ./ratex-svg
