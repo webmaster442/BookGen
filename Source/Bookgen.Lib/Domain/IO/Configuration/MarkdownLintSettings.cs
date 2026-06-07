af24a1b6a5669cff18defb1cb93b2a381ce5b833
webmaster442 <ruzsinszki.gabor@gmail.com>
2026-06-07 17:35:19 +0200

    Remove linting and try an other approach

--- Source/Bookgen.Lib/Domain/IO/Configuration/MarkdownLintSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel;
-
-namespace Bookgen.Lib.Domain.IO.Configuration;
-
-public sealed class MarkdownLintSettings
-{
-    [Description("Heading levels should only increment by one level at a time")]
-    public bool Md001 { get; set; } = true;
-}
