3fdd072f54dd7aa51f4f00361ae0e917629e5af4
webmaster442 <ruzsinszki.gabor@gmail.com>
2026-06-19 15:57:20 +0200

    Fix tests

---
+++ Test/Bookgen.Tests/Lib/UT_AppSettingsAccessor.cs
@@ -0,0 +1,36 @@
+﻿using Bookgen.Lib.Confighandling;
+
+using BookGen.Vfs;
+
+using Moq;
+
+namespace Bookgen.Tests.Lib;
+
+[TestFixture]
+public class UT_AppSettingsAccessor
+{
+    private Mock<IWritableFileSystem> _fileSystem;
+    private AppSettingsAccessor _sut;
+
+    [SetUp]
+    public void Setup()
+    {
+        _fileSystem = new Mock<IWritableFileSystem>(MockBehavior.Strict);
+        _fileSystem.Setup(x => x.FileExists(It.IsAny<string>())).Returns(false);
+        _sut = new AppSettingsAccessor(_fileSystem.Object);
+    }
+
+    [Test]
+    public void EnsureThat_GetWorks()
+    {
+        var value = _sut.Get(x => x.Editor);
+        bool isValid = _sut.IsSettingValid(x => x.Editor, out IReadOnlyList<string> issues);
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(value, Is.EqualTo("notepad.exe"));
+            Assert.That(isValid, Is.True);
+            Assert.That(issues, Is.Empty);
+        }
+    }
+}
