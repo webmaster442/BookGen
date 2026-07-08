ca14a1b789292d388d0547adddde7b922e00a49a
webmaster442 <ruzsinszki.gabor@gmail.com>
2026-07-08 20:35:06 +0200

    Template engine test adaptation

--- Test/Bookgen.Tests/Lib/UT_TemplateEngine.cs
+++ Test/Bookgen.Tests/Lib/UT_TemplateEngine.cs
@@ -7,8 +7,6 @@ using BookGen.Lib.Rendering.Templates;
 
 using BookGen.Vfs;
 
-using Microsoft.Extensions.Logging;
-
 using Moq;
 
 namespace Bookgen.Tests.Lib;
@@ -20,23 +18,49 @@ internal class UT_TemplateEngine
     private TestLogger _logger;
     private Mock<IAssetSource> _assetSourceMock;
 
+    public class TestTimeProvider : TimeProvider
+    {
+        public override DateTimeOffset GetUtcNow()
+        {
+            return new DateTimeOffset(2026, 1, 1, 11, 12, 13, TimeSpan.Zero);
+        }
+
+        public override TimeZoneInfo LocalTimeZone
+            => TimeZoneInfo.Utc;
+    }
+
+
     [SetUp]
     public void Setup()
     {
         _assetSourceMock = new Mock<IAssetSource>(MockBehavior.Strict);
         _logger = new TestLogger();
-        _sut = new TemplateEngine(_logger, _assetSourceMock.Object);
-        _sut.RegisterFunction("MyFunction", (arg) => string.Join(',', arg));
+        _sut = new TemplateEngine(_logger, _assetSourceMock.Object, new TemplateEngineOptions
+        {
+            TimeProvider = new TestTimeProvider()
+        });
     }
 
     [TestCase("", "")]
-    [TestCase("<p>No template tags</p>", "<p>No template tags</p>\r\n")]
-    [TestCase("<h1>{{Title}}</h1><p>{{Content}}</p>", "<h1>title</h1><p>This is content</p>\r\n")]
-    [TestCase("<h1>{{Title}}</h1><p>{{MyFunction(\"Foo\")}}</p>", "<h1>title</h1><p>Foo</p>\r\n")]
-    [TestCase("{{MyFunction(\"Foo\")}}", "Foo\r\n")]
-    [TestCase("{{MyFunction(\"Foo\", \"Bar\")}}", "Foo,Bar\r\n")]
-    [TestCase("{{MyFunction(\"Foo\", \"Bar\",\"Baz\")}}", "Foo,Bar,Baz\r\n")]
-    [TestCase("{{MyFunction(\"Foo\", \"Bar\",\"Baz\", \"Banana\")}}", "Foo,Bar,Baz,Banana\r\n")]
+    [TestCase("<p>No template tags</p>", "<p>No template tags</p>")]
+    [TestCase("<h1>{{Title}}</h1><p>{{Content}}</p>", "<h1>title</h1><p>This is content</p>")]
+    [TestCase("{{LastModified}}", "1987-10-11")]
+    [TestCase("{{ToUpper(Title)}}", "TITLE")]
+    [TestCase("{{ToLower(Title)}}", "title")]
+    [TestCase("{{Substring(Content, 0, 4)}}", "This")]
+    [TestCase("{{Trim('  Hello  ')}}", "Hello")]
+    [TestCase("{{TrimStart('  Hello  ')}}", "Hello  ")]
+    [TestCase("{{TrimEnd('  Hello  ')}}", "  Hello")]
+    [TestCase("{{Replace(Content, 'content', 'Universe')}}", "This is Universe")]
+    [TestCase("{{Concat(Content, '-','Bar', '-', '42')}}", "This is content-Bar-42")]
+    [TestCase("{{Concat(Content, ' ', 42)}}", "This is content 42")]
+    [TestCase("{{HtmlEncode('<div>')}}", "&lt;div&gt;")]
+    [TestCase("{{UrlEncode('https://example.com')}}", "https%3A%2F%2Fexample.com")]
+    [TestCase("{{UrlDecode('https%3A%2F%2Fexample.com')}}", "https://example.com")]
+    [TestCase("{{CurrentDate()}}", "2026-01-01")]
+    [TestCase("{{CurrentTime()}}", "11:12:13")]
+    [TestCase("{{CurrentDateTime()}}", "2026-01-01 11:12:13")]
+    [TestCase("{{RegexReplace(Content, 's', 'z')}}", "Thiz iz content")]
     public void EnsureThat_Render_Works(string template, string expected)
     {
         var viewData = new ViewData
@@ -54,7 +78,7 @@ internal class UT_TemplateEngine
     }
 
     [Test]
-    public void EnsureThat_Render_Warns_Unrecognized_Template_Parts()
+    public void EnsureThat_Render_Errors_Unrecognized_Template_Parts()
     {
         var viewData = new ViewData
         {
@@ -63,15 +87,16 @@ internal class UT_TemplateEngine
             Host = string.Empty,
             LastModified = new DateTime(1987, 10, 11)
         };
+
         string template = "<h1>{{Title}}</h1><p>{{UnrecognizedPart}}</p>";
 
         _sut.Render(template, viewData);
 
-        Assert.That(_logger.Warnings, Is.EqualTo(1));
+        Assert.That(_logger.Errors, Is.EqualTo(1));
     }
 
     [Test]
-    public void EnsureThat_Render_Warns_Unrecognized_Functions()
+    public void EnsureThat_Render_Errors_Unrecognized_Functions()
     {
         var viewData = new ViewData
         {
@@ -82,6 +107,6 @@ internal class UT_TemplateEngine
         };
         string template = "<h1>{{Title}}</h1><p>{{UnrecognizedFunction()}}</p>";
         _sut.Render(template, viewData);
-        Assert.That(_logger.Warnings, Is.EqualTo(1));
+        Assert.That(_logger.Errors, Is.EqualTo(1));
     }
 }
