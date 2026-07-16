6869f75fbbdfd8547ca1aacd691a89cf84a68b7c
webmaster442 <ruzsinszki.gabor@gmail.com>
2026-07-16 16:39:23 +0200

    Work on Sample plugin

---
+++ Source/BookGen.SamplePlugin/Helpers.cs
@@ -0,0 +1,25 @@
+﻿namespace BookGen.SamplePlugin;
+
+internal static class Helpers
+{
+    public static uint GetId(string filePath)
+    {
+        uint result = 2166136261;
+        foreach (char c in filePath)
+        {
+            result ^= c;
+            result *= 16777619;
+        }
+        return result;
+    }
+
+    public static string ReadEmbeddedFile(string fileName)
+    {
+        using Stream stream = typeof(Helpers).Assembly.GetManifestResourceStream(fileName)
+            ?? throw new InvalidOperationException($"Embedded resource '{fileName}' not found.");
+
+        using var reader = new StreamReader(stream);
+
+        return reader.ReadToEnd();
+    }
+}
--- Source/BookGen.SamplePlugin/SamplePlugin.cs
+++ Source/BookGen.SamplePlugin/SamplePlugin.cs
@@ -1,6 +1,4 @@
-﻿using System.Text;
-
-using BookGen.Api;
+﻿using BookGen.Api;
 using BookGen.Api.V1;
 
 namespace BookGen.SamplePlugin;
@@ -11,42 +9,70 @@ public sealed class SamplePlugin : IBuildPluginV1
     {
         HtmlBuilder contentHtml = new(32 * 1024);
         HtmlBuilder navHtml = new(4 * 1024);
-
+        
         IRenderer renderer = bookgenServices.CreateRenderer(new RendererOptions
         {
             SvgRecode = RendererOptions.ImageOption.Passtrough,
         });
 
-        await AddToContentHtml(contentHtml, book.Index, renderer);
+        bookgenServices.Logger.LogDebug("Processing document: {DocumentPath}", book.Index.FilePath);
+        (string content, IDocumentFrontMatter frontMatter) indexData = await book.Index.ReadContent();
+
+        string IndexHtml = renderer.RenderMarkdownToRawHtml(indexData.content);
+
+        Add(contentHtml, navHtml, indexData, renderer);
 
         foreach (IChapter chapter in book.Chapters)
         {
             foreach (IDocument document in chapter.Documents)
             {
-                await AddToContentHtml(contentHtml, document, renderer);
+                bookgenServices.Logger.LogDebug("Processing document: {DocumentPath}", document.FilePath);
+                (string content, IDocumentFrontMatter frontMatter) docData = await document.ReadContent();
+                Add(contentHtml, navHtml, docData, renderer);
             }
         }
+
+        if (navHtml.LastUnclosedTag != null)
+            navHtml.CloseOpenTag();
+
+        RenderTags tags = new()
+        {
+            Content = contentHtml.ToString(),
+            Title = indexData.frontMatter.Title,
+            AdditionalData = new()
+            {
+                { "Navigation", navHtml.ToString() },
+                { "Index", IndexHtml }
+            }
+        };
+
+        string template = Helpers.ReadEmbeddedFile("BookGen.SamplePlugin.Template.html");
+
+        string rendered = renderer.RenderMarkdownToHtml(template, tags);
+
+        await bookgenServices.OutputFolder.WriteTextFile("rendered.html", rendered);
 
         return true;
     }
 
-    private async Task AddToContentHtml(HtmlBuilder contentDivs, IDocument document, IRenderer renderer)
+    private void Add(HtmlBuilder contentHtml,
+                     HtmlBuilder navHtml,
+                     (string content, IDocumentFrontMatter frontMatter) docData,
+                     IRenderer renderer)
     {
-        (string content, _) = await document.ReadContent();
+        contentHtml.Element("div",
+                    tag => tag.Id(Helpers.GetId(docData.frontMatter.Title).ToString()),
+                    div => div.Raw(renderer.RenderMarkdownToRawHtml(docData.content)));
 
-        contentDivs.Element("div",
-                            tag => tag.Id(GetId(document.FilePath).ToString()),
-                            div => div.Raw(renderer.RenderMarkdownToRawHtml(content)));
-    }
-
-    private static uint GetId(string filePath)
-    {
-        uint result = 2166136261;
-        foreach (char c in filePath)
+        if (navHtml.LastUnclosedTag == "ul")
         {
-            result ^= c;
-            result *= 16777619;
+            navHtml.Element("li",
+                li => li.Element("a", tag => tag.Attr("href", "#"),
+                a => a.Text(docData.frontMatter.Title)));
         }
-        return result;
+        else
+        {
+            navHtml.Element("ul");
+        }
     }
 }
