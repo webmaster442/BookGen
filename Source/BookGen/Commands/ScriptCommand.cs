5cf1fdb30fa7d710e274f49202ccf983072954a7
webmaster442 <ruzsinszki.gabor@gmail.com>
2026-08-07 18:23:58 +0200

    Scripts specification

---
+++ Source/BookGen/Commands/ScriptCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using BookGen.Cli;
+
+namespace BookGen.Commands;
+
+internal sealed class ScriptCommand : AsyncCommand<ScriptCommand.Arguments>
+{
+    public sealed class Arguments : ArgumentsBase
+    {
+
+    }
+
+    public override Task<int> ExecuteAsync(Arguments arguments, IReadOnlyList<string> context, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+}
