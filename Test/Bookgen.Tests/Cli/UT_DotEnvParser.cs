9f6865f2ac98d840359aca2235a3be1a7a1558ca
webmaster442 <ruzsinszki.gabor@gmail.com>
2026-08-08 15:19:17 +0200

    License headers and solution update

--- Test/Bookgen.Tests/Cli/UT_DotEnvParser.cs
+++ Test/Bookgen.Tests/Cli/UT_DotEnvParser.cs
@@ -1,4 +1,9 @@
-﻿using BookGen.Cli.Dotenv;
+﻿//-----------------------------------------------------------------------------
+// (c) 2019-2026 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using BookGen.Cli.Dotenv;
 
 namespace Bookgen.Tests.Cli;
 
