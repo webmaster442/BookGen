570090e514b5ac0c7d072bbcca21fb9a0dcdaec5
webmaster442 <ruzsinszki.gabor@gmail.com>
2026-08-25 17:23:10 +0200

    JS engine fix

--- Source/BookGen.Lib/Rendering/Markdown/RenderInterop/JavascriptEngine.cs
+++ Source/BookGen.Lib/Rendering/Markdown/RenderInterop/JavascriptEngine.cs
@@ -34,12 +34,13 @@ internal sealed class JavascriptEngine : IDisposable
     public void SetVariable(string name, string value)
     {
         ObjectDisposedException.ThrowIf(_disposed, nameof(_engine));
+        string encoded = System.Text.Encodings.Web.JavaScriptEncoder.Default.Encode(value);
         string script = $$"""
             if ({{name}} === 'undefined') {
-                var {{name}} = `{{value}}`;
+                var {{name}} = "{{encoded}}";
             }
             else {
-                {{name}} = `{{value}}`;
+                {{name}} = "{{encoded}}";
             }
             """;
         _engine.Execute(script);
